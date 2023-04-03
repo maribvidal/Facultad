8d0a36f1c67b64627a19aa6214036f302bbca392
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-04-03 18:44:30 -0300

    Subí un ejercicio

---
+++ Primer Semestre/CADP/Práctica 2/Ejercicio6a.pas
@@ -0,0 +1,27 @@
+{Realice un módulo que lea del teclado números enteros hasta que llegue un valor negativo. 
+Al finalizar la lectura, el módulo debe imprimir en pantalla cuál fue el número par más alto.}
+program ejercicio6a;
+function lecturaNumeros: integer;
+    var
+        numIngresado, numPar: integer;
+    begin
+        {Inicializar}
+        numPar:= 0;
+        repeat
+            write('Ingrese un número: ');
+            read(numIngresado);
+            if (numIngresado mod 2) = 0 then {Si el número ingresado, si es divido 2, da resto 0...}
+                begin
+                if (numIngresado > numPar) then {Si el número ingresado es mayor al numPar actual...}
+                    begin
+                        numPar:= numIngresado; {Actualizar la variable numPar}
+                    end;
+                end;
+        until (numIngresado < 0); {Repetir por última vez cuando el número ingresado sea negativo}
+        write('El número par mas alto fue: ',numPar);
+        lecturaNumeros:= numPar; {Se obtiene el mayor número par al llamar la función}
+    end;
+begin
+    lecturaNumeros;
+    {No le asigno una variable a la función lecturaNumeros porque la función de por sí hace todo el trabajo}
+end.
