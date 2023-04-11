0eae4ef518407ffac1fe52b49e60c9c1ee28624c
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-04-10 22:06:36 -0300

    Incompleto

---
+++ Primer Semestre/CADP/Práctica 2/Capítulo 2/ejercicio8.pas
@@ -0,0 +1,32 @@
+program ejercicio8;
+function esPar(numero: integer):boolean;
+    begin
+        esPar:= (numero mod 2) = 0;
+    end;
+procedure calcularDigitos(secuencia: integer; var sumaDigitosPares, cantidadDigitosImpares: integer);
+    var
+        digito: integer;
+    begin
+        while (secuencia <> 0) do begin
+            digito:= secuencia mod 10;
+            if (esPar(digito)) then
+                sumaDigitosPares:= sumaDigitosPares + digito
+            else
+                cantidadDigitosImpares:= cantidadDigitosImpares + 1;
+            secuencia:= secuencia div 10;
+        end;
+    end;
+var
+    sec, sumDigPar, cantDigImpar: integer;
+begin
+    sumDigPar:= 0; cantDigImpar:= 0;
+    write('Ingresar secuencia de números enteros: '); read(sec);
+    while (sec <> 123456) do begin
+        writeln(' ------------------------------------ ');
+        calcularDigitos(sec, sumDigPar, cantDigImpar);
+        writeln('Suma de los digitos pares de la secuencia: ',sumDigPar);
+        writeln('Cantidad de los digitos impares de la secuencia: ',cantDigImpar);
+        write('Ingresar secuencia de números enteros: '); readln(sec);
+        sumDigPar:= 0; cantDigImpar:= 0;
+    end;
+end.
