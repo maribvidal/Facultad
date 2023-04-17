bb65cac53f1d61091692dc0fe262a3b73311da69
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-04-17 19:56:26 -0300

    Corregido

--- Primer Semestre/CADP/Práctica 3/Ejercicio3.pas
+++ Primer Semestre/CADP/Práctica 3/Ejercicio3.pas
@@ -16,49 +16,49 @@ procedure leerColegio(var s: school); {Ingresar los datos de cada colegio}
         write('>Ingrese la cantidad de alumnos: '); readln(s.cAlum);
         write('>Ingrese la localidad: '); readln(s.localidad);
     end;
-procedure actualizarMaximos(s: school; var cueMax1, cueMax2: integer; var nomMax1, nomMax2: string; var promMax1, promMax2: real);
+procedure actualizarMaximos(s: school; var cueMin1, cueMin2: integer; var nomMin1, nomMin2: string; var promMin1, promMin2: real);
     begin
-        if (relacionDA(s.cAlum, s.cDoc) < promMax1) then {Si la relación docente-alumno es mayor que el primer promedio grande...}
+        if (relacionDA(s.cAlum, s.cDoc) < promMin1) then {Si la relación docente-alumno es menor que el primer promedio...}
         begin
-            cueMax2:= cueMax1;
-            nomMax2:= nomMax1;
-            promMax2:= promMax1;
+            cueMin2:= cueMin1;
+            nomMin2:= nomMin1;
+            promMin2:= promMin1;
             {---}
-            cueMax1:= s.CUE;
-            nomMax1:= s.nombre;
-            promMax1:= relacionDA(s.cAlum, s.cDoc);
+            cueMin1:= s.CUE;
+            nomMin1:= s.nombre;
+            promMin1:= relacionDA(s.cAlum, s.cDoc);
         end
-        else if (relacionDA(s.cAlum, s.cDoc) < promMax2) then {Si, en cambio, es mayor que el segundo promedio grande...}
+        else if (relacionDA(s.cAlum, s.cDoc) < promMin2) then {Si, en cambio, es menor que el segundo promedio...}
         begin
-            cueMax2:= s.CUE;
-            nomMax2:= s.nombre;
-            promMax2:= relacionDA(s.cAlum, s.cDoc);                
+            cueMin2:= s.CUE;
+            nomMin2:= s.nombre;
+            promMin2:= relacionDA(s.cAlum, s.cDoc);                
         end;
     end;
 const
     propUNESCO = 23.435;
 var
     colegio: school;
-    i, cantEsp, cueMax1, cueMax2: integer;
-    nomMax1, nomMax2: string;
-    promMax1, promMax2: real;
+    i, cantEsp, cueMin1, cueMin2: integer;
+    nomMin1, nomMin2: string;
+    promMin1, promMin2: real;
 begin
     {Inicializar variables}
     cantEsp:= 0;
-    cueMax1:= 0;
-    cueMax2:= 0;
-    nomMax1:= ' ';
-    nomMax2:= ' ';
-    promMax1:= 9999;
-    promMax2:= 9999;
+    cueMin1:= 0;
+    cueMin2:= 0;
+    nomMin1:= ' ';
+    nomMin2:= ' ';
+    promMin1:= 9999;
+    promMin2:= 9999;
     {Programa principal}
-    for i:= 1 to 1 do {Hay que leer 2400 escuelas}
+    for i:= 1 to 2400 do {Hay que leer 2400 escuelas}
     begin
         leerColegio(colegio);
         if ((colegio.localidad = 'La Plata') and (relacionDA(colegio.cAlum, colegio.cDoc) > propUNESCO)) then cantEsp:= cantEsp + 1;
-        actualizarMaximos(colegio, cueMax1, cueMax2, nomMax1, nomMax2, promMax1, promMax2);
+        actualizarMaximos(colegio, cueMin1, cueMin2, nomMin1, nomMin2, promMin1, promMin2);
     end;
     writeln('>Cantidad de escuelas de La Plata con una relación de alumnos por docente superior a la sugerida por UNESCO: ', cantEsp);
-    writeln('>El primer colegio con mejor relación docente-alumno es el ',nomMax1,' y su CUE es ',cueMax1);
-    write('>El segundo colegio con mejor relación docente-alumno es el ',nomMax2,' y su CUE es ',cueMax2);
+    writeln('>El primer colegio con mejor relación docente-alumno es el ',nomMin1,' y su CUE es ',cueMin1);
+    write('>El segundo colegio con mejor relación docente-alumno es el ',nomMin2,' y su CUE es ',cueMin2);
 end.
