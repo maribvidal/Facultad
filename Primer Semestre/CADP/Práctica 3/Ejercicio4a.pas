496dc8d0e31b6d8b5a6f8e3cdaf3f422e5cb0e22
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-04-18 14:12:40 -0300

    Terminado

--- Primer Semestre/CADP/Práctica 3/Ejercicio4a.pas
+++ Primer Semestre/CADP/Práctica 3/Ejercicio4a.pas
@@ -12,6 +12,32 @@ procedure leerTelefono(var l: linea);
         write('>Ingresar la cantidad de minutos consumidos en un mes: '); readln(l.minutos);
         write('>Ingresar la cantidad de MBs consumidos en un mes: '); readln(l.MB);
     end;
+procedure procesarCliente(var facturaMinutos, facturaMB: real);
+    var 
+        i: integer;
+        l: linea;
+        c: cliente;
+    begin
+        write('>Ingresar el código del cliente: '); readln(c.codigo);
+        write('>Ingresar la cantidad de líneas que posee: '); readln(c.lineas);
+        for i:= 1 to c.lineas do
+        begin
+            leerTelefono(l);
+            facturaMinutos:= facturaMinutos + l.minutos;
+            facturaMB:= facturaMB + l.MB;
+        end;
+        facturaMinutos:= facturaMinutos * 3.4 ;
+        facturaMB:= facturaMB * 1.35;
+    end;
+var 
+    i: integer;
+    facturaMinutos, facturaMB: real;
 begin
-    {Inicializar variables}
+    facturaMinutos:= 0;
+    facturaMB:= 0;
+    for i:= 1 to 9300 do {Leer 9300 clientes}
+    begin
+        procesarCliente(facturaMinutos, facturaMB);
+        writeln('El cliente tiene que pagar ',facturaMinutos:2:2,'$ por los minutos y ',facturaMB:2:2,' por los MB gastados.');
+    end;
 end.
