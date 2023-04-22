45025022bf0cb8383ccc40d06a95da4740a836a1
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-04-22 14:31:13 -0300

    Actualizado incompleto

--- Primer Semestre/CADP/Práctica 3/Ejercicio11.pas
+++ Primer Semestre/CADP/Práctica 3/Ejercicio11.pas
@@ -2,7 +2,7 @@ program Ejercicio11;
 type
     //Limitar el rango de los valores a ingresar
     rangoTxt = string[40]
-    rangoNum = 0..99999;
+    rangoNum = -1..99999;
     vuelo = record //Crear registro para el vuelo
         codigo: rangoNum;
         pais_salida: rangoTxt;
@@ -46,18 +46,49 @@ procedure actualizarAvPaises(var avPaisesViejo, codAvPaisesViejo, avPaisesNuevo,
         end;
     end;
 procedure procesarVuelos(var maxmin: codMaxMin; var avPaisesDif, cantEsp1, cantEsp2: integer; var seguir: boolean);
+    var
+        v: vuelo;
+        codigoActual, avPaisesNuevo, avPaisesViejo, codAvPaisesNuevo, codAvPaisesViejo: integer;
+        paisActual: string;
     begin
-        //Vacío
+        //Inicializar variables
+        avPaisesNuevo:= 0; codAvPaisesNuevo:= 0;
+        avPaisesViejo:= 0; codAvPaisesViejo:= 0;
+        //Ingresar los dos datos que deciden si se cortan las cadenas o no
+        write('Ingrese el país de salida >:'); readln(v.pais_salida);
+        write('Ingrese el código del avión >:'); readln(v.codigo);
+        codigoActual:= v.codigo;
+        paisActual:= v.pais_salida;
+        //Comenzar la cadena
+        while (v.codigo <> cortar) do begin //Romper la cadena cuando el código ingresado sea igual a 44
+            //--
+            while (v.pais_salida = paisActual) do begin //Romper la cadena si el pais ingresado es diferente al paisActual
+                //--
+                while (v.codigo = codigoActual) do begin //Romper la cadena si el codigo ingresado es diferente al codigoActual
+                    //--
+                end;
+            end;
+        end;
     end;
 var
     seguir: boolean;
     maxmin: codMaxMin;
     avPaisesDif, cantEsp1, cantEsp2: integer;
 begin
+    //Inicializar variables
+    maxmin.codMax1:= 0; maxmin.cantMax1:= -1;
+    maxmin.codMax2:= 0; maxmin.cantMax2:= -1;
+    maxmin.codMin1:= 0; maxmin.cantMin1:= 99999;
+    maxmin.codMin2:= 0; maxmin.cantMin2:= 99999;
+    avPaisesDif:= 0;
+    cantEsp1:= 0; cantEsp2:= 0;
+    seguir:= true;
+    //Comenzar el proceso
     while (seguir) do begin
         procesarVuelos(maxmin, avPaisesDif, cantEsp1, cantEsp2, seguir);
         writeln(' ----- ');
     end;
+    //Informar
     writeln('Los dos aviones que mas kilometros recorrieron son los ',maxmin.codMax1,' y ',maxmin.codMax2);
     writeln('Los dos aviones que menos kilometros recorrieron son los ',maxmin.codMin1,' y ',maxmin.codMin2);
     writeln('La cantidad de vuelos de más de 5.000 km que no alcanzaron el 60% de ocupación del avión: ',cantEsp1);
