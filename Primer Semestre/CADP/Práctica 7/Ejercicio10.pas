28b77bc71a37e84c7cabb03b3a06ff12512bd368
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-06-09 09:13:06 -0300

    Actualizado - corregido

--- Primer Semestre/CADP/Práctica 7/Ejercicio10.pas
+++ Primer Semestre/CADP/Práctica 7/Ejercicio10.pas
@@ -44,10 +44,11 @@ procedure leerEmpresa(var e: empresa);
             write('Nombre de la ciudad donde está radicada: '); readln(e.ciudad_radicada);
             leerCultivo(c);
             i:= 1;
+            e.dimL:= 0;
             while (i <= dimF) and (c.hect <> cultivo_corte) do begin
+                e.dimL:= e.dimL + 1;
                 e.cultivos[i]:= c;
                 leerCultivo(c);
-                e.dimL:= e.dimL + 1;
                 i:= i + 1;
             end;
         end;
@@ -82,79 +83,47 @@ function poseeDosCeros(codigo: integer):boolean;
         while (codigo <> 0) do begin
             if ((codigo mod 10) = 0) then
                 ceros:= ceros + 1;
+            codigo:= codigo div 10;
         end;
         poseeDosCeros:= (ceros = 2);
     end;
 //b. Nombres de las empresas radicadas en “San Miguel del Monte” que cultivan trigo y cuyo código de empresa posee al menos dos ceros.
-procedure puntoB(l: lista);
+procedure puntoBCDE(l: lista);
     var
+        empresaActual, empresaMax: str20;
         condicion: boolean;
-        i: integer;
-    begin
-        while (l <> nil) do begin
-            condicion:= false;
-            for i:= 1 to l^.data.dimL do begin
-                if (l^.data.cultivos[i].tipo = 'trigo') then
-                    condicion:= true;
-            end;
-            if (l^.data.ciudad_radicada = 'San Miguel del Monte') and (poseeDosCeros(l^.data.codigo)) and (condicion) then
-                writeln(l^.data.nombre);
-            l:= l^.sig;
-        end;
-    end;
-//La cantidad de hectáreas dedicadas al cultivo de soja y qué porcentaje representa con respecto al total de hectáreas
-procedure puntoC(l: lista);
-    var
-        i, hectSoja, hectTotal: integer;
+        i, hectSoja, hectTotal, tiempoActual, tiempoMax: integer;
     begin
         hectSoja:= 0;
         hectTotal:= 0;
-        while (l <> nil) do begin
-            for i:= 1 to l^.data.dimL do begin
-                if (l^.data.cultivos[i].tipo = 'soja') then
-                    hectSoja:= hectSoja + l^.data.cultivos[i].hect;
-                hectTotal:= hectTotal + l^.data.cultivos[i].hect;
-            end;
-            l:= l^.sig;
-        end;
-        writeln('qué porcentaje representa con respecto al total de hectáreas: ',((hectSoja/hectTotal)*100):2:2,'% de ',hectTotal);
-    end;
-//La empresa que dedica más tiempo al cultivo de maíz
-procedure puntoD(l: lista);
-    var
-        empresaActual, empresaMax: str20;
-        i, tiempoActual, tiempoMax: integer;
-    begin
         empresaMax:= '';
         tiempoMax:= -1;
         while (l <> nil) do begin
             empresaActual:= l^.data.nombre;
             tiempoActual:= 0;
+            condicion:= false;
             for i:= 1 to l^.data.dimL do begin
+                if (l^.data.cultivos[i].tipo = 'trigo') then
+                    condicion:= true;
                 if (l^.data.cultivos[i].tipo = 'maiz') then
                     tiempoActual:= tiempoActual + l^.data.cultivos[i].meses;
+                if (l^.data.tipo = 'PRIVADA') and (l^.data.cultivos[i].tipo = 'girasol') and (l^.data.cultivos[i].hect < 5) then
+                    l^.data.cultivos[i].meses:= l^.data.cultivos[i].meses + 1;
+                    writeln('TIEMPO AUMENTADO: de ',l^.data.cultivos[i].meses-1,' a ',l^.data.cultivos[i].meses);
+                if (l^.data.cultivos[i].tipo = 'soja') then
+                    hectSoja:= hectSoja + l^.data.cultivos[i].hect;
+                hectTotal:= hectTotal + l^.data.cultivos[i].hect;
             end;
+            if (l^.data.ciudad_radicada = 'San Miguel del Monte') and (poseeDosCeros(l^.data.codigo)) and (condicion) then
+                writeln(l^.data.nombre);
             if (tiempoActual > tiempoMax) then begin
                 tiempoMax:= tiempoActual;
                 empresaMax:= empresaActual;
             end;
             l:= l^.sig;
         end;
+        writeln('qué porcentaje representa con respecto al total de hectáreas: ',((hectSoja/hectTotal)*100):2:2,'% de ',hectTotal);
         writeln('La empresa que dedica más tiempo al cultivo de maíz: ',empresaMax);
-    end;
-//Realizar un módulo que incremente en un mes los tiempos de cultivos de girasol de menos de 5 hectáreas de todas las empresas que no son estatales.
-procedure puntoE(l: lista);
-    var
-        i: integer;
-    begin
-        while (l <> nil) do begin
-            for i:= 1 to l^.data.dimL do begin
-                if (l^.data.tipo = 'PRIVADA') and (l^.data.cultivos[i].tipo = 'girasol') and (l^.data.cultivos[i].hect < 5) then
-                    l^.data.cultivos[i].meses:= l^.data.cultivos[i].meses + 1;
-                writeln('TIEMPO AUMENTADO: de ',l^.data.cultivos[i].meses-1,' a ',l^.data.cultivos[i].meses);
-            end;
-            l:= l^.sig;
-        end;   
     end;
 var
     l: lista;
@@ -162,8 +131,8 @@ begin
     //Tenía paja y no pasé ninguna variable al programa principal, está todo informado desde los respectivos módulos XD
     l:= nil;
     cargarLista(l);
-    puntoB(l);
-    puntoC(l);
-    puntoD(l);
-    puntoE(l);
+    if (l <> nil) then
+        puntoBCDE(l)
+    else
+        write('No se cargó nada');
 end.
