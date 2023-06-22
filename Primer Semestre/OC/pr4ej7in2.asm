deca175b5b2b27a0ec1beb6de83416d8c9e2fea9
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-06-22 18:21:23 -0300

    subido

--- Primer Semestre/OC/pr4ej7in2.asm
+++ Primer Semestre/OC/pr4ej7in2.asm
@@ -1,5 +1,5 @@
 org 1000h
-varA db 06h
+varA db 09h
 varB db 08h
 varC db ?
 
@@ -11,15 +11,15 @@ cmp al, bl; hacer la cuenta
 jz : cero; si la operación dió cero, cumplió con A <= B, por lo que C = A
 jnz : no_cero; si no dió cero, quedan dos posibilidades, o A < B, o A > B
 cero: mov cl, varA; asignar primer resultado (C = A)
-jmp final; terminar el programa
+ jmp final; terminar el programa
 no_cero: nop; no hacer nada durante esta línea de código
-js : priRes
-jns : segRes
-priRes : nop
-mov cl, varA
-jmp final
-segRes : nop
-mov cl, varB
-jmp final
+ js : priRes; si el ult. valor calc. es negativo, es porque A < B
+ jns : segRes; si no, es porque A > B
+priRes: nop; C = A
+ mov cl, varA
+ jmp final
+segRes: nop; C = B
+ mov cl, varB
+ jmp final
 final: hlt
 end
