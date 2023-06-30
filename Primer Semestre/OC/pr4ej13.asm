bd82a27662cf6d3ba4f0e73c39313aab7634fd62
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-06-30 20:16:45 -0300

    Actualizado

--- Primer Semestre/OC/pr4ej13.asm
+++ Primer Semestre/OC/pr4ej13.asm
@@ -1,38 +1,36 @@
-;Dada la frase "Organización y la Computación", 
-;almacenada en la memoria, escriba un programa que determine
-;cuantas letras ‘a’ seguidas de ‘c’ hay en ella.
 ORG 1000H
-CANTIDAD DB ?
+CANT DB ?
 INICIO DB "Organización y la Computación"; Lleva las letras en la tabla
 FIN DB ?
 
 ORG 2000h
-MOV BX, OFFSET INICIO + 1; Asignarle a BX la dirección  inicial de la tabla (comenzando por las letras)
-MOV AL, OFFSET INICIO - FIN; Calcular longitud de la tabla
+MOV BX, OFFSET INICIO; Asignarle a BX la dirección inicial de la tabla
 MOV CL, 0; Guardar la cantidad de 'A's seguidas de 'C's
 MOV DL, 0; Acumulador de los espacios sin A
 RECORRER: NOP; Este módulo va a repetirse hasta que se haya leido toda la tabla
- CMP BYTE PTR [BX], 061h; Comprobar si es una 'a'
-  JZ : SI_ES_A
-  JNZ: INC DL
+ MOV DH, [BX]; Mover la letra al registro DH
+ CMP DH, 061h; Comprobar si es una 'a'
+  JZ : SI_A
+  INC DL
   JMP PASAR
-  SI_ES_A: MOV DL, 0
+  SI_A: MOV DL, 0
  PASAR: NOP
- CMP BYTE PTR [BX], 063h; Comprobar si es una 'c'
+ CMP DH, 063h; Comprobar si es una 'c'
   JNZ : PASAR2; Si no es 'c', entonces pasar de largo
-  CMP DL, 0; Si DL es Z=1 entonces significa que A está al lado
+  CMP DL, 1; Si DL es Z=1 entonces significa que A está al lado
    JNZ : PASAR2; En caso contrario, pasar de largo.
    INC CL
  PASAR2: NOP
- CMP BYTE PTR [BX], 043h; Comprobar si es una 'C'
+ CMP DH, 043h; Comprobar si es una 'C'
   JNZ : PASAR3; Si no es 'C', entonces pasar de largo
-  CMP DL, 0; Si DL es Z=1 entonces significa que A está al lado
+  CMP DL, 1; Si DL es Z=1 entonces significa que A está al lado
    JNZ : PASAR3; En caso contrario, pasar de largo.
    INC CL
  PASAR3: NOP
- DEC AL; Sacarle un número 
+ CMP BX, OFFSET FIN; Sacarle un número 
  JZ : FINAL; Si ya no quedan mas elementos... terminar programa
  INC BX; Próxima dirección
  JMP RECORRER; LOOP
-FINAL: HLT
+FINAL: MOV CANT, AL
+HLT
 END
