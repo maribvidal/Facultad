3bf6cbe3487d4b428975b00ae18c7f0062080705
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-09-03 19:53:17 -0300

    Actualizado

--- Segundo semestre/Taller/Modulo Imperativo/Práctica Recursiva/ej6.pas
+++ Segundo semestre/Taller/Modulo Imperativo/Práctica Recursiva/ej6.pas
@@ -1,21 +1,17 @@
 program ej6pr2;
 procedure binarizar(num:integer);
-    var
-        aux: integer;
     begin
         if (num <> 0) then begin
-            aux:= num; //Guardar el número de manera temporal para despues poder imprimir el resto
-            num:= num div 2;
-            binarizar(num);
-            write(aux mod 2);
+            binarizar(num div 2);
+            write(num mod 2);
         end;
     end;
 var
     num: integer;
 begin
-    num:= -1;
+    write('Ingrese un número: '); readln(num);
     while (num <> 0) do begin //Mientras el número ingresado sea diferente de 0
-        write('Ingrese un número: '); readln(num);
         binarizar(num); writeln;
+        write('Ingrese un número: '); readln(num);
     end;
 end.
