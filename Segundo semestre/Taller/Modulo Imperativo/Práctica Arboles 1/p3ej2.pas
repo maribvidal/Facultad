6336f77bb0d2d570208ac7935c874627bc6258af
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-09-07 20:35:37 -0300

    Actualizado

--- Segundo semestre/Taller/Modulo Imperativo/Práctica Arboles 1/p3ej2.pas
+++ Segundo semestre/Taller/Modulo Imperativo/Práctica Arboles 1/p3ej2.pas
@@ -1,6 +1,3 @@
-//b. Implemente un módulo que reciba el árbol generado en i. y un código de producto y retorne
-//la cantidad total de unidades vendidas de ese producto.
-
 program ej2pr3;
 const
     corte = 0;
@@ -46,13 +43,12 @@ procedure agregarArbol2(var a: arbol; data: venta); //Inciso II
             a^.HD:= nil;
             a^.data:= data;
         end else begin
-            if (data.codigo = a^.data.codigo) then begin
-                a^.data.unidades:= a^.data.unidades + data.unidades;
+            if (data.codigo < a^.data.codigo) then begin
+                agregarArbol2(a^.HI, data);
+            end else if (data.codigo > a^.data.codigo) then begin
+                agregarArbol2(a^.HD, data);
             end else begin
-                if (data.codigo <= a^.data.codigo) then
-                    agregarArbol(a^.HI, data)
-                else
-                    agregarArbol(a^.HD, data);
+                a^.data.unidades:= a^.data.unidades + data.unidades;
             end;
         end;
     end;
@@ -100,26 +96,48 @@ function retornarVentas(a: arbol; codigo, contador: integer):integer;
         end;
         retornarVentas:= contador;
     end;
-procedure codigoVentas(a: arbol; codigo: integer; var contador: integer);
+function puntoB(a: arbol; codigo: integer):integer;
+    var
+        cant: integer;
     begin
-        if (a <> nil) then begin
-            if (codigo < a^.data.codigo) then begin
-                retornarVentas(a^.HI, codigo, contador);
-            end else if (codigo > a^.data.codigo) then begin
-                retornarVentas(a^.HD, codigo, contador);
-            end else if (codigo = a^.data.codigo) then begin
-                writeln('pene3');
-                contador:= contador + a^.data.unidades;
+        cant:= 0; //Inicializar variable en 0
+        if (a <> nil) then begin //Si el arbol no está vacío...
+            if (codigo < a^.data.codigo) then begin //Si el código es menor que el del nodo actual del arbol...
+                cant:= puntoB(a^.HI, codigo); //Ir al hijo izquierdo
+            end else if(codigo > a^.data.codigo) then begin//Si el código es mayor que el del nodo actual del arbol...
+                cant:= puntoB(a^.HD, codigo); //Ir al hijo derecho
+            end else begin //Si no (osea si es igual) entonces
+                cant:= a^.data.unidades; //Asignarle a cant las unidades del nodo actual
+                cant:= cant + puntoB(a^.HI, codigo); //Y sumarle las que hayan del lado izquierdo
+                cant:= cant + puntoB(a^.HD, codigo); //Y sumarle las que hayan del lado derecho
             end;
         end;
+        puntoB:= cant; //Retornar cantidad de unidades vendidas
+    end;
+function puntoC(a2: arbol; codigo: integer):integer;
+    var
+        cant: integer;
+    begin
+        cant:= 0; //Inicializar variable en 0
+        if (a2 <> nil) then begin //Si el arbol no está vacío...
+            if (codigo < a2^.data.codigo) then begin //Si el código es menor que el del nodo actual del arbol...
+                cant:= puntoC(a2^.HI, codigo); //Ir al hijo izquierdo
+            end else if(codigo > a2^.data.codigo) then begin//Si el código es mayor que el del nodo actual del arbol...
+                cant:= puntoC(a2^.HD, codigo); //Ir al hijo derecho
+            end else begin //Si no (osea si es igual) entonces
+                cant:= a2^.data.unidades; //Asignarle a cant las unidades del nodo actual
+            end;
+        end;
+        puntoC:= cant; //Retornar cantidad de unidades vendidas
     end;
 var
-    a: arbol;
-    contador: integer;
+    a, a2: arbol;
 begin
+    a:= nil;
+    a2:= nil;
     cargarArbol(a);
+    cargarArbol2(a2);
     mirarArbol(a);
-    contador:= 0;
-    codigoVentas(a, 10, contador);
-    writeln('El código ',10,' tuvo ', contador);
+    writeln('El código ',10,' tuvo ', puntoB(a, 10));
+    writeln('El código ',5,' tuvo ', puntoC(a2, 5));
 end.
