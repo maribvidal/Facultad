bcfa9fdc4a7715130d7e437b35eb31e32df86ce9
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-09-12 10:59:51 -0300

    Terminado

--- Segundo semestre/Arquitectura de Computadoras/Práctica 2/ej14.asm
+++ Segundo semestre/Arquitectura de Computadoras/Práctica 2/ej14.asm
@@ -12,19 +12,54 @@ ORG 44; Enlazar subrutina a la interrupción
   DW RUT_CLK
 
 ORG 1000H
-  SEG DB ?
+  SEG DB 30H
+      DB 30H
 
 ORG 3000H
   RUT_F10: NOP
     INC CH; Incrementar CH
-    MOV AL, 20H
+    CMP CH, VECES
+    JNZ PASAR_3
+    MOV CH, 0; Reiniciar CH
+  CMP DH, 1;
+  JNZ PASAR
+    MOV DL, 1
+    MOV DH, 0
+    MOV AL, 0
+    OUT TIMER, AL; Resetear timer
+    JMP PASAR_3
+  PASAR: CMP DL, 1
+    JNZ PASAR_3
+    MOV DL, 0
+    MOV DH, 1
+PASAR_3: MOV AL, 20H
     OUT PIC, AL; Notificarle al EOI que se terminó la interrupción
     IRET
 
 ORG 3500H
   RUT_CLK: NOP
-    
-    MOV AL, 20H
+    CMP DL, 1; Verificar si se puede contar
+    JNZ NO_PUEDO
+      INC SEG+1
+      CMP SEG+1, 3AH
+      JNZ NADA_1
+        MOV SEG+1, 30H
+        INC SEG
+        CMP SEG, 33H
+        JNZ NADA_1
+          MOV AL, 2
+          INT 7; Imprimirlo antes que nada
+          MOV DL, 0
+          MOV DH, 1
+          MOV SEG, 30H
+          MOV SEG+1, 30H
+          JMP NADA_2
+NADA_1: NOP
+  MOV AL, 2
+  INT 7
+NADA_2: MOV AL, 0
+  OUT TIMER, AL
+NO_PUEDO: MOV AL, 20H
     OUT PIC, AL
     IRET
 
@@ -49,22 +84,10 @@ CLI; DESHABILITAR INTERRUPCIONES
   OUT TIMER+1, AL; Inicializar registro COMP en 1
   MOV AL, 0
   OUT TIMER, AL; Inicializar registro CONT en 0
+  MOV BX, OFFSET SEG
 STI; VOLVER A HABILITAR INTERRUPCIONES
 ;Loop
-LOOP: NOP
-  CMP DH, 1;
-  JNZ PASAR
-    MOV DL, 1
-    MOV DH, 0
-    JMP PASAR_2
-  PASAR: CMP DL, 1
-    JNZ PASAR_2
-    MOV DL, 0
-    MOV DH, 1
-  PASAR_2: CMP CH, VECES
-    JNZ PASAR_3
-    MOV CH, 0; Reiniciar CH
-  PASAR_3: JMP LOOP
+LOOP: JMP LOOP
 ;Devolver registros
 POP DX
 POP CX
