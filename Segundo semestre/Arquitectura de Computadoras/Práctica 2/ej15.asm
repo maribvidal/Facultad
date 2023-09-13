065553036eb3f95548b9f814f72c238a01d333a4
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-09-13 18:25:58 -0300

    Actualizar

--- Segundo semestre/Arquitectura de Computadoras/Práctica 2/ej15.asm
+++ Segundo semestre/Arquitectura de Computadoras/Práctica 2/ej15.asm
@@ -1,35 +1,48 @@
+;CONSTANTES
 TIMER EQU 10H
 PIC EQU 20H
 EOI EQU 20H
 ID_F10 EQU 10
-ID_CLK EQU 20
+ID_CLK EQU 11
+CERO EQU 30H
+SEIS EQU 36H
+DIEZ EQU 3AH
 
-ORG 40
+ORG 40; Handler del interruptor del F10
   DW RUT_F10
 
-ORG 44
+ORG 44; Handler del interruptor del TIMER
   DW RUT_CLK
 
 ORG 1000H
 NUM DB ?
+MSJ DB "INGRESE UN NUMERO: "
 SEG DB 30H
     DB 30H
+    DB 7CH; Espacio
 
 ORG 3000H
-  RUT_F10: INC CL
-  CMP CL, 1
+  RUT_F10: INC CL; Incrementar CL
+  CMP CL, 1; Restar CL con 1
     JNZ PASAR_3
     MOV CL, 0
-      CMP CH, 1
+      CMP CH, 1; Restar CH con 1
       JNZ PASAR_2
-        MOV CH, 0
+        MOV CH, 0; Switch
         MOV DL, 1
-        MOV BX, OFFSET NUM
-        INT 6
+        ;RECIBIR UN NÚMERO DEL USUARIO
+        MOV BX, OFFSET MSJ; Mensaje
+        MOV AL, OFFSET SEG - OFFSET MSJ; Longitud
+        INT 7; Impresión
+        MOV BX, OFFSET NUM; Dirección de cadena
+        INT 6; Recibir
+        ;SETEAR RELOJ
+        MOV AL, 0
+        OUT TIMER, AL
         JMP PASAR_3
       PASAR_2: CMP DL, 1
       JNZ PASAR_3
-        MOV CH, 1
+        MOV CH, 1; Switch
         MOV DL, 0
         JMP PASAR_3
     PASAR_3: MOV AL, EOI
@@ -38,24 +51,62 @@ ORG 3000H
 
 ORG 3500H
   RUT_CLK: NOP
+  ;VERIFICAR SI SE PUEDE CONTAR
+  CMP DL, 1
+  JNZ CLK_PASAR
+      ;INCREMENTAR EL SEGUNDO DE LA IZQ.
+      INC SEG+1
+      CMP SEG+1, DIEZ
+      JNZ CLK_PASAR
+          ;SI LLEGÓ A DIEZ ENTONCES REINICIAR EL SEGUNDO E INCREMENTAR EL PRIMER SEG
+          MOV SEG+1, CERO
+          INC SEG; Incrementar el primer SEG
+          CMP SEG, SEIS
+          JNZ CLK_PASAR
+              ;SI EL SEGUNDO SEG LLEGÓ A SEIS ENTONCES REINICIARLO
+              MOV SEG, CERO
+              JMP CLK_PASAR; Saltar a la etiqueta
+  ;PASAR DE LARGO
+  CLK_PASAR: MOV AL, 2; Leer 3 celdas
+  MOV BX, OFFSET SEG; Comenzar a leer desde la dir. de SEG
+  INT 7
+  ;REINICIAR CONT
+  MOV AL, 0
+  OUT TIMER, AL
+  ;TERMINAR INTERRUPCIÓN
   MOV AL, EOI
   OUT PIC, AL
   IRET
 
 ORG 2000H
 CLI
+  ;VAMOS A USAR ESTOS REGISTROS
+  PUSH CX;
+  PUSH BX;
+  PUSH AX;
+  PUSH DX
+  ;INICIALIZAR CX Y DX
+  MOV CX, 0100h
+  MOV DX, 0
+  ;RESTO DEL PROGRAMA
   MOV AL, 0FCH
-  OUT PIC+1, AL
+  OUT PIC+1, AL; IMR
   MOV AL, ID_F10
-  OUT PIC+4, AL
+  OUT PIC+4, AL; 24h - INT0
   MOV AL, ID_CLK
-  OUT PIC+5, AL
+  OUT PIC+5, AL; 25h - TIMER
   MOV AL, 1
   OUT TIMER+1, AL; Inicializar registro COMP en 1
   MOV AL, 0
   OUT TIMER, AL; Inicializar registro CONT en 0
   MOV BX, OFFSET SEG
 STI
+;LOOP
 LOOP: JMP LOOP
+;DEVOLVER REGISTROS
+POP DX
+POP CX
+POP BX
+POP AX
 INT 0
 END
