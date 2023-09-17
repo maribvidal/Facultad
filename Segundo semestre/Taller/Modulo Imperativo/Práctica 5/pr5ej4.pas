860d43aff63e3406a5c8442ca69c157dede9f68a
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-09-17 15:41:11 -0300

    Actualizado

--- Segundo semestre/Taller/Modulo Imperativo/Práctica 5/pr5ej4.pas
+++ Segundo semestre/Taller/Modulo Imperativo/Práctica 5/pr5ej4.pas
@@ -26,12 +26,23 @@ type
         HI: arbol;
         HD: arbol;
     end;
-    
+    listaCodigos = ^nodoCodigos;
+    nodoCodigos = record
+        num: integer;
+        sig: listaCodigos;
+    end;
+procedure imprimirListaRec(l: listaReclamos);
+    begin
+        while (l <> nil) do begin
+            writeln('Código: ',l^.data.codigo,' / Año: ',l^.data.ano,' / Tipo: ',l^.data.tipo);
+            l:= l^.sig;
+        end;
+    end;
 procedure leerReclamo(var rec: reclamo);
     begin
         write('Introducir código >'); readln(rec.codigo);
         if (rec.codigo <> codigo_corte) then begin
-            rec.DNI:= Random(100) + 1; writeln('DNI generado >',rec.DNI);
+            rec.DNI:= Random(15) + 1; writeln('DNI generado >',rec.DNI);
             rec.ano:= Random(50) + 2000; writeln('Año generado >',rec.ano);
             rec.tipo:= Random(10) + 1; writeln('Tipo de reclamo generado >',rec.tipo);
         end;
@@ -47,18 +58,24 @@ procedure agregarNodo(var l: listaReclamos; rec: reclamo);
     end;
 procedure agregarArbol(var arb: arbol; rec: reclamo);
     begin
-        if (arb <> nil) then begin
+        if (arb = nil) then begin
             new(arb);
             arb^.elem.lis:= nil;
+            agregarNodo(arb^.elem.lis, rec);
             arb^.elem.DNI:= rec.dni;
             arb^.elem.cantTotal:= 1;
             arb^.HI:= nil;
             arb^.HD:= nil;
         end else begin
-            if (arb^.elem.dni > rec.dni) then
-                agregarArbol(arb^.HI, rec)
-            else
+            if (arb^.elem.dni > rec.dni) then begin
+                agregarArbol(arb^.HI, rec);
+            end else if (arb^.elem.dni < rec.dni) then begin
                 agregarArbol(arb^.HD, rec);
+            end else begin
+                agregarNodo(arb^.elem.lis, rec);
+                writeln('Nodo agregado');
+                arb^.elem.cantTotal:= arb^.elem.cantTotal + 1;
+            end;
         end;
     end;
 procedure cargarArbol(var arb: arbol);
@@ -73,39 +90,100 @@ procedure cargarArbol(var arb: arbol);
         end;
     end;
 //Punto B
-function reclamosCont(l: listaReclamos):integer;
-    var
-        contador: integer;
+procedure reclamosDNI(arb: arbol; DNI: integer; var cant: integer);
     begin
-        contador:= 0;
-        if (l <> nil) then begin
-            contador:= contador + 1;
-            contador:= contador + reclamosCont(l^.sig);
-        end;
-        reclamosCont:= contador;
-    end;
-function reclamosDNI(arb: arbol; DNI: integer):integer;
-    var
-        cant: integer;
-    begin
-        cant:= 0;
         if (arb <> nil) then begin
-            if (arb^.elem.DNI < DNI) then begin
-                reclamosDNI(arb^.HI, DNI)   
-            end else if (arb^.elem.DNI > DNI) then begin
-                reclamosDNI(arb^.HD, DNI);
+            if (arb^.elem.DNI > DNI) then begin
+                reclamosDNI(arb^.HI, DNI, cant);
+            end else if (arb^.elem.DNI < DNI) then begin
+                reclamosDNI(arb^.HD, DNI, cant);
             end else begin
-                cant:= reclamosCon(arb^.elem.lis);
+                cant:= arb^.elem.cantTotal;
             end;
         end;
-        reclamosDNI:= cant;
     end;
 //Punto C
 procedure dniEntre(arb: arbol; inf, sup: integer; var cont: integer);
+    begin
+        if (arb <> nil) then begin
+            if (arb^.elem.dni >= inf) then begin
+                if (arb^.elem.dni <= sup) then begin
+                    writeln('Cantidad total del nodo: ',arb^.elem.cantTotal);
+                    cont:= cont + arb^.elem.cantTotal;
+                    writeln('Cont actualizado: ',cont);
+                    dniEntre(arb^.HI, inf, sup, cont);
+                    dniEntre(arb^.HD, inf, sup, cont);
+                end else begin
+                    dniEntre(arb^.HI, inf, sup, cont);
+                end;
+            end else begin
+                dniEntre(arb^.HD, inf, sup, cont);
+            end;
+        end;
+    end;
+//Punto D
+procedure agregarNodoCodigo(var l: listaCodigos; num: integer);
+    var
+        aux: listaCodigos;
+    begin
+        new(aux);
+        aux^.num:= num;
+        aux^.sig:= l;
+        l:= aux;
+    end;
+procedure codigosAno(arb: arbol; ano: integer; var l: listaCodigos);
+    begin
+        if (arb <> nil) then begin //No hay búsqueda eficiente :'c
+            while (arb^.elem.lis <> nil) do begin
+                if (arb^.elem.lis^.data.ano = ano) then
+                    agregarNodoCodigo(l, arb^.elem.lis^.data.codigo);
+                arb^.elem.lis:= arb^.elem.lis^.sig;
+            end;
+            codigosAno(arb^.HI, ano, l);
+            codigosAno(arb^.HD, ano, l);
+        end;
+    end;
+procedure imprimirLista(l: listaCodigos);
+    begin
+        while (l <> nil) do begin
+            writeln('Código: ',l^.num);
+            l:= l^.sig;
+        end;
+    end;
+procedure imprimirArbol(arb: arbol);
+    begin
+        if (arb <> nil) then begin
+            imprimirArbol(arb^.HI);
+            writeln('DNI: ',arb^.elem.DNI);
+            imprimirListaRec(arb^.elem.lis);
+            writeln;
+            imprimirArbol(arb^.HD);
+        end;
+    end;
 //Programa principal
 var
-
+    arb: arbol;
+    listaCod: listaCodigos;
+    numInput, numInput2, cont: integer;
 begin
+    arb:= nil;
+    listaCod:= nil;
+    numInput:= 0;
+    numInput2:= 0;
+    cont:= 0;
     Randomize;
-
+    cargarArbol(arb);
+    imprimirArbol(arb);
+    write('Ingrese un DNI: '); readln(numInput);
+    reclamosDNI(arb, numInput, cont);
+    writeln('El DNI ',numInput,' hizo ',cont,' reclamos');
+    write('Ingrese DNI inferior: '); readln(numInput);
+    write('Ingrese DNI superior: '); readln(numInput2);
+    cont:= 0;
+    dniEntre(arb, numInput, numInput2, cont);
+    writeln('Entre el DNI ',numInput,' y el ',numInput2,' se hicieron ',cont,' reclamos');
+    write('Ingrese un año: '); readln(numInput);
+    codigosAno(arb, numInput, listaCod);
+    writeln('LISTA CON LOS CÓDIGOS DE LOS RECLAMOS HECHOS EN EL AÑO ',numInput);
+    imprimirLista(listaCod);
 end.
