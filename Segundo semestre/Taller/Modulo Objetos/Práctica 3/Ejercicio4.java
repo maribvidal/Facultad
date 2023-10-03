e47cc4fe9ecb0c4e8f445b6e17e51dfa046aa5f8
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-10-03 18:02:25 -0300

    Subido - preguntar

---
+++ Segundo semestre/Taller/Modulo Objetos/Práctica 3/Ejercicio4.java
@@ -0,0 +1,35 @@
+package tema3;
+
+import PaqueteLectura.Lector;
+
+public class Ej04Hotel {
+    public static void main(String[] args) {
+        int i, cliEdad, n = 5;
+        String cliNombre, cliDNI;
+        Hotel hotel = new Hotel(n);
+        Cliente c;
+        
+        for (i = 0; i < n; i++) {
+            //Leer información del cliente
+            System.out.println("Nombre del cliente: ");
+            cliNombre = Lector.leerString();
+            System.out.println("DNI del cliente: ");
+            cliDNI = Lector.leerString();
+            System.out.println("Edad del cliente: ");
+            cliEdad = Lector.leerInt();
+            
+            //Crear cliente
+            c = new Cliente(cliNombre, cliDNI, cliEdad);
+            
+            //Meter cliente al hotel
+            hotel.meterCliente(i, c);
+        }
+        
+        //Mostrar el hotel
+        System.out.println(hotel.toString());
+        
+        //Aumentar el precio de todas las habitaciones y mostrar hotel nuevamente
+        hotel.aumentarPrecio(50);
+        System.out.println(hotel.toString());
+    }
+}
