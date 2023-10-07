f9153c60934df30a10427d73afbf8221287b8864
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-10-07 14:58:54 -0300

    Subido

---
+++ Segundo semestre/Taller/Modulo Objetos/Práctica Repaso/Ejercicio 1/Investigador.java
@@ -0,0 +1,62 @@
+package repaso_ej1;
+
+public class Investigador {
+    private String nombre;
+    private int categoria;
+    private String especialidad;
+    
+    private Subsidio [] vec_subsidios = new Subsidio [5];
+    private int vec_dimL = 0;
+
+    //Constructor
+    public Investigador(String nombre, int categoria, String especialidad) {
+        this.setNombre(nombre);
+        this.setCategoria(categoria);
+        this.setEspecialidad(especialidad);
+    }
+    
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+    public int getCategoria() {
+        return categoria;
+    }
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    //Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+    
+    //Otros métodos
+    public void agregarSubsidio(Subsidio unSubsidio) {
+        if (this.vec_dimL < 5) {
+            this.vec_subsidios[this.vec_dimL] = unSubsidio;
+            this.vec_dimL++;
+        } else {
+            System.out.println("ERROR: No pueden entrar mas elementos en el vector");
+        }
+    }
+    public double getDineroOtorgado() {
+        double aux = 0;
+        for (int i = 0; i < this.vec_dimL; i++) {
+            if (this.vec_subsidios[i].getOtorgado()) {
+                aux += this.vec_subsidios[i].getMonto();
+            }
+        }
+        return aux;
+    }
+    public void otorgarSubsidios() {
+        for (int i = 0; i < this.vec_dimL; i++)
+            this.vec_subsidios[i].setOtorgado(true);
+    }
+}
