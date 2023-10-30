e3c169ace3dd7f87c229a8c99885c8bb1e690556
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-10-30 18:31:31 -0300

    Corregido

--- Segundo semestre/Arquitectura de Computadoras/Práctica 5/ej8.s
+++ Segundo semestre/Arquitectura de Computadoras/Práctica 5/ej8.s
@@ -1,6 +1,6 @@
 .data
 	cadena1: .asciiz "winmips"
-	cadena2: .asciiz "winmivs"
+	cadena2: .asciiz "winmips"
 	longitud: .word 7
 	coincidencias: .word 0
 	pos: .word 0
@@ -11,15 +11,15 @@
 	sd $v0, pos($zero)
 	halt
 
-analizarCadena: daddi $t3, $zero, 0; Para contar las coincidencias
-	lazo:	ld $t1, cadena1($a0); Cadena 1
-		ld $t2, cadena2($a0); Cadena 2
-		bne $t1, $t2, saltar; Si $t1 =/= $t2
-			daddi $t3, $t3, 1; Incrementar la cant. de coincidencias
+analizarCadena: daddi $t1, $zero, 0; Para contar las coincidencias
+	lazo:	lbu $t2, cadena1($a0); Cadena 1
+		lbu $t3, cadena2($a0); Cadena 2
+		bne $t2, $t3, saltar; Si $t2 =/= $t3
+			daddi $t1, $t1, 1; Incrementar la cant. de coincidencias
 	saltar: daddi $a0, $a0, 1; Ir al próximo carácter
 		bne $a0, $a1, lazo; Si el desplazamiento no es igual que la longitud...
-		bne $t3, $a1, saltar2
-			daddi $v0, $zero, -1; Setear $v0 en -1
-			j terminar
+		bne $t1, $a1, saltar2
+		daddi $v0, $zero, -1; Setear $v0 en -1
+		j terminar
 	saltar2: dadd $v0, $zero, $a0
 	terminar: jr $ra
