d954448f39387a5791c29cd1cdf8cb667c9b4d20
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-11-01 19:00:34 -0300

    Subido

---
+++ Segundo semestre/Arquitectura de Computadoras/Práctica 5/ej12-norecursivo-.s
@@ -0,0 +1,16 @@
+.data
+	valor: .word 10
+	result: .word 0
+.text
+	daddi $sp, $zero, 0x400
+	ld $a0, valor($zero)
+	jal factorial
+	sd $v0, result($zero)
+	halt
+
+factorial: daddi $v0, $zero, 1
+	daddi $t0, $zero, 1
+lazo:	dmul $v0, $v0, $a0
+	bne $a0, $t0, lazo
+	daddi $a0, $a0, -1
+	jr $ra
