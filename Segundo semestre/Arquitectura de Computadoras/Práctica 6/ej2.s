66bd6210f13937dffc6284612dbd085e982fbe59
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2023-11-07 18:26:34 -0300

    Actualizado

--- Segundo semestre/Arquitectura de Computadoras/Práctica 6/ej2.s
+++ Segundo semestre/Arquitectura de Computadoras/Práctica 6/ej2.s
@@ -15,8 +15,7 @@
 	;INICIALIZAR PUERTO DE E/S y la pila
 	lwu $s0, dirControl($0)
 	lwu $s1, dirData($0)
-	daddi $sp, $0, 0x400
-	daddi $sp, $sp, -16
+	daddi $sp, $0, 0x3F0
 	sd $s1, 0($sp) ;Guardar valores en la pila
 	sd $s0, 8($sp)
 	jal ingreso
@@ -25,59 +24,25 @@
 	daddi $sp, $sp, 16
 	halt
 
-ingreso:	ld $s1, 0($sp)
-		ld $s0, 8($sp)
-ciclo:		daddi $t0, $0, 0;  Para hacer slti
-		daddi $t1, $0, 8; NÚMERO DE INGRESO
-		sd $t1, 0($s0); Pedir dato
-		ld $v0, 0($s1); Recibir dato
+ingreso:	ld $s0, 8($sp)
+		ld $s1, 0($sp)
+ciclo:		daddi $t0, $0, 0
+		daddi $t1, $0, 8
+		sd $t1, 0($s0)
+		ld $v0, 0($s1)
 		slti $t0, $v0, 10
 		beqz $t0, ciclo
 		slti $t0, $v0, 0
 		bnez $t0, ciclo
-		jr $ra; Se verificó que $v0 fuera un valor
+		jr $ra
 
 muestra:	ld $s0, 8($sp)
 		ld $s1, 0($sp)
-		daddi $t1, $0, 0
-		slti $t1, $a0, 1
-		beqz $t1, num_1
+		daddi $t1, $0, 8
+		dmul $t1, $t1, $a0
 		daddi $t0, $0, str_cero
-		j terminar
-num_1:		slti $t1, $a0, 2
-		beqz $t1, num_2
-		daddi $t0, $0, str_uno
-		j terminar
-num_2:	slti $t1, $a0, 3
-	beqz $t1, num_3
-	daddi $t0, $0, str_dos
-	j terminar
-num_3:		slti $t1, $a0, 4
-		beqz $t1, num_4
-		daddi $t0, $0, str_tres
-		j terminar
-num_4:	slti $t1, $a0, 5
-	beqz $t1, num_5
-	daddi $t0, $0, str_cuatro
-	j terminar
-num_5:		slti $t1, $a0, 6
-		beqz $t1, num_6
-		daddi $t0, $0, str_cinco
-		j terminar
-num_6:	slti $t1, $a0, 7
-	beqz $t1, num_7
-	daddi $t0, $0, str_seis
-	j terminar
-num_7:		slti $t1, $a0, 8
-		beqz $t1, num_8
-		daddi $t0, $0, str_siete
-		j terminar
-num_8:	slti $t1, $a0, 9
-	beqz $t1, num_9
-	daddi $t0, $0, str_ocho
-	j terminar
-num_9:	daddi $t0, $0, str_nueve
-terminar: sd $t0, 0($s1)
-	daddi $t0, $0, 4; CÓDIGO DE LECTURA DE STRING
-	sd $t0, 0($s0)
-	jr $ra	
+		dadd $t0, $t0, $t1
+		sd $t0, 0($s1)
+		daddi $t0, $0, 4
+		sd $t0, 0($s0)
+		jr $ra
