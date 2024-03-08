5f6560edc6e327aadc5e20ae4a4eb697c854d2d3
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-03-08 13:05:25 -0300

    Subido

---
+++ Tercer semestre/Seminario .NET/Práctica 1/ej15.cs
@@ -0,0 +1,9 @@
+// Ejercicio 15 - Práctica 1
+using System;
+
+//Se soluciona quitando la declaración de I que se hace dentro del paréntesis del for
+int i = 0;
+for (i = 1; i <= 10;)
+{
+    Console.WriteLine(i++);
+}
