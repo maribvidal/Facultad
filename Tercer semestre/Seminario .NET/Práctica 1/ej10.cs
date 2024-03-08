b7b7b357349800340852480f94cd3d3933a799c8
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-03-08 16:56:56 -0300

    Actualizado

--- Tercer semestre/Seminario .NET/Práctica 1/ej10.cs
+++ Tercer semestre/Seminario .NET/Práctica 1/ej10.cs
@@ -1,21 +1,7 @@
-// Ejercicio 9 - Práctica 1
 using System;
 
-//Declarar variables
-int contador = 1;
-
-//Mientras número sea menor o igual que 1000...
-while (contador <= 1000) {
-    //Comprobar si numero es un múltiplo de 17
-    if (contador % 17 == 0) {
-        Console.WriteLine(contador + " es un múltiplo de 17");
-    }
-
-    //Comprobar si numero es un múltiplo de 29
-    if (contador % 29 == 0) {
-        Console.WriteLine(contador + " es un múltiplo de 29");
-    }
-
-    //Incrementar el contador
-    contador++;
+for (int i = 1; i <= 1000; i++) 
+{
+  if (i % 17 == 0) Console.WriteLine(i + " es múltiplo de 17");
+  if (i % 29 == 0) Console.WriteLine(i + " es múltiplo de 29");
 }
