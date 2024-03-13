23b9eaf2f4f83506c04554013754e13a88b414c1
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-03-13 19:04:09 -0300

    Subido

---
+++ Tercer semestre/Fundamentos de Organización de Datos/Práctica 1/ej3a.pas
@@ -0,0 +1,48 @@
+{3. Realizar un programa que presente un menú con opciones para:
+a. Crear un archivo de registros no ordenados de empleados y completarlo con
+datos ingresados desde teclado. De cada empleado se registra: número de
+empleado, apellido, nombre, edad y DNI. Algunos empleados se ingresan con
+DNI 00. La carga finaliza cuando se ingresa el String ‘fin’ como apellido.}
+
+program pr1ej3a;
+
+const
+	STR_CORTE = 'fin';
+	NOMBRE_ARCHIVO = 'empleado.dat';
+
+type
+	tipoEmpleado = record
+		nro: integer;
+		apellido: string[15];
+		nombre: string[15];
+		edad: integer;
+		DNI: integer;
+	end;
+	archEmpleado = file of tipoEmpleado;
+	
+var
+	archivo: archEmpleado;
+	input: tipoEmpleado;
+
+procedure leerEmpleado(var emp: tipoEmpleado);
+	begin
+		write('APELLIDO DEL EMPLEADO: '); readln(emp.apellido);
+		if (emp.apellido <> STR_CORTE) then begin
+			write('NOMBRE DEL EMPLEADO: '); readln(emp.nombre);
+			write('EDAD DEL EMPLEADO: '); readln(emp.edad);
+			write('DNI DEL EMPLEADO: '); readln(emp.DNI);
+			write('NUMERO DEL EMPLEADO: '); readln(emp.nro);
+		end;
+	end;
+
+begin
+	Assign(archivo, NOMBRE_ARCHIVO);
+	rewrite(archivo);
+	leerEmpleado(input);
+	while (input.apellido <> STR_CORTE) do begin
+		write(archivo, input);
+		writeln();
+		leerEmpleado(input);
+	end;
+	close(archivo);
+end.
