4176dca28621ded69532fcfdb8240f20b07138b6
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-03-13 19:29:16 -0300

    Actualizado

--- Tercer semestre/Fundamentos de Organización de Datos/Práctica 1/ej2.pas
+++ Tercer semestre/Fundamentos de Organización de Datos/Práctica 1/ej2.pas
@@ -1,68 +1,89 @@
-{Realizar un algoritmo, que utilizando el archivo de números enteros no ordenados
-creado en el ejercicio 1, informe por pantalla cantidad de números menores a 1500 y el
-promedio de los números ingresados. El nombre del archivo a procesar debe ser
-proporcionado por el usuario una única vez. Además, el algoritmo deberá listar el
-contenido del archivo en pantalla.}
+{3. Realizar un programa que presente un menú con opciones para:
+b. Abrir el archivo anteriormente generado y
+i. Listar en pantalla los datos de empleados que tengan un nombre o apellido
+determinado, el cual se proporciona desde el teclado.
+ii. Listar en pantalla los empleados de a uno por línea.
+iii. Listar en pantalla los empleados mayores de 70 años, próximos a jubilarse.}
 
-program pr1ej2;
+program pr1ej3b;
 
-Uses sysutils;
-
-{CONSTANTES}
-const
-	NUM_CORTE = 30000;
-	NUM_ESP = 1500;
-
-{TIPOS}
 type
-	archivo_numero = file of integer;
+	tipoEmpleado = record
+		nro: integer;
+		apellido: string[15];
+		nombre: string[15];
+		edad: integer;
+		DNI: longint;
+	end;
+	archEmpleado = file of tipoEmpleado;
 
-{MÉTODOS}
-procedure debug(var archivo: archivo_numero);
-	var
-		tipo_dato: integer;
+procedure imprimirEmpleado(emp: tipoEmpleado);
 	begin
-		{Ir al principio del archivo}
-		seek(archivo, 0);
-		while not eof(archivo) do begin
-			read(archivo, tipo_dato);
-			write(tipo_dato, ' ');
+		write('N°', emp.nro);
+		write(' / DNI: ', emp.DNI);
+		write(' / NOMBRE COMPLETO DEL EMPLEADO: ', emp.nombre, ' ',emp.apellido);
+		write(' / EDAD: ', emp.edad);
+	end;
+
+procedure imprimirPorNomApe(var arch: archEmpleado; nombre, apellido: string);
+	var
+		emp_aux: tipoEmpleado;
+	begin
+		{Colocar puntero de elemento en el principio}
+		seek(arch, 0);
+		{Buscar si existe}
+		while not eof(arch) do begin
+			read(arch, emp_aux);
+			if (emp_aux.nombre = nombre) or (emp_aux.apellido = apellido) then
+				imprimirEmpleado(emp_aux); 
 		end;
 	end;
 
-{VARIABLES}
+procedure imprimirContenido(var arch: archEmpleado);
+	var
+		emp_aux: tipoEmpleado;
+	begin
+		seek(arch, 0);
+		while not eof(arch) do begin
+			read(arch, emp_aux);
+			imprimirEmpleado(emp_aux);
+		end;
+	end;
+	
+procedure imprimirPorEdad(var arch: archEmpleado);
+	var
+		emp_aux: tipoEmpleado;
+	begin
+		seek(arch, 0);
+		while not eof(arch) do begin
+			read(arch, emp_aux);
+			if (emp_aux.edad > 70) then
+				imprimirEmpleado(emp_aux);
+		end;
+	end;
+	
 var
-	num_arch: archivo_numero;
-	num_input, num_prom, num_veces: integer;
-	str_input: string;
+	archivo: archEmpleado;
+	input: string[15];
+	input2: string[15];
+	nomArch: string;
 
-{PROGRAMA PRINCIPAL}
 begin
-	{INICIALIZACIÓN}
-	num_prom:= 0;
-	num_veces:= 0;
-	
-	{CREAR ARCHIVO}
-	write('INGRESE EL NOMBRE DEL ARCHIVO: '); readln(str_input);
-	Assign(num_arch, str_input); {Asignar nombre lógico a un archivo físico}
-	rewrite(num_arch); {Crearlo y, por lo tanto, solo hacer operaciones de escritura}
-	
-	{CARGAR NÚMEROS}
-	write('INGRESE UN NÚMERO: '); readln(num_input);
-	while (num_input <> NUM_CORTE) do begin
-		write(num_arch, num_input);
-		num_prom:= num_prom + num_input;
-		if (num_input < NUM_ESP) then begin
-			num_veces:= num_veces + 1;
-		end;
-		write('INGRESE UN NÚMERO: '); readln(num_input);
-	end;
-	
-	{IMPRIMIR EL CONTENIDO DEL ARCHIVO EN PANTALLA}
-	writeln('Numeros inferiores a 1500: ', num_veces);
-	writeln('Promedio de los números ingresados: ', (num_prom/filesize(num_arch)):2:0);
-	debug(num_arch);
-	
-	{CERRAR ARCHIVO}
-	close(num_arch);
+	write('INGRESE EL NOMBRE DEL ARCHIVO: '); readln(nomArch);
+	Assign(archivo, nomArch);
+	reset(archivo);
+	{Inciso I}
+	write('INGRESE UN NOMBRE A BUSCAR: '); readln(input);
+	write('INGRESE UN APELLIDO A BUSCAR: '); readln(input2);
+	writeln();
+	writeln('LOS SIGUIENTES EMPLEADOS CUMPLEN CON LAS CARACTERISTICAS');
+	imprimirPorNomApe(archivo, input, input2);
+	writeln();
+	{Inciso II}
+	imprimirContenido(archivo);
+	writeln();
+	{Inciso III}
+	writeln('log');
+	imprimirPorEdad(archivo);
+	close(archivo);
 end.
