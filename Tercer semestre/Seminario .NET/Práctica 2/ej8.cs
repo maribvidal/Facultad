699688aa8b093c73d561fd946936e79e4d7d1390
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-03-15 17:32:10 -0300

    Subido

--- Tercer semestre/Seminario .NET/Práctica 2/ej8.cs
+++ Tercer semestre/Seminario .NET/Práctica 2/ej8.cs
@@ -1,1 +1,13 @@
+using System;
+using System.Text;
 
+StringBuilder myString = new StringBuilder("Una secuencia de numeros: ");
+int i = 0;
+
+while (i < 100)
+{
+    myString.Append(i.ToString() + "");
+    i++;
+}
+
+Console.WriteLine(myString);
