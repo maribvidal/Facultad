97f0ddf829808e937d6e9b470583ea915a664ab7
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-03-19 16:21:32 -0300

    Terminado

--- Tercer semestre/Fundamentos de Organización de Datos/Práctica 1/ej6.pas
+++ Tercer semestre/Fundamentos de Organización de Datos/Práctica 1/ej6.pas
@@ -49,9 +49,9 @@ procedure crearArchivo(var arch: archivocel);
 		reset(archivo_texto);
 		
 		while not eof(archivo_texto) do begin
-			readln(archivo_texto, cel_aux.codigo, cel_aux.nombre, cel_aux.desc);
-			readln(archivo_texto, cel_aux.marca, cel_aux.precio, cel_aux.stock_min);
-			readln(archivo_texto, cel_aux.stock_disp);
+			readln(archivo_texto, cel_aux.codigo, cel_aux.stock_min, cel_aux.desc);
+			readln(archivo_texto, cel_aux.stock_disp, cel_aux.precio, cel_aux.nombre);
+			readln(archivo_texto, cel_aux.marca);
 			write(arch, cel_aux);
 		end;
 		
@@ -100,9 +100,9 @@ procedure exportarArchivo(var arch: archivocel);
 		rewrite(archivo_texto);
 		while not eof(arch) do begin
 			read(arch, cel_aux);
-			writeln(archivo_texto, cel_aux.codigo, cel_aux.nombre, cel_aux.desc);
-			writeln(archivo_texto, cel_aux.marca, cel_aux.precio, cel_aux.stock_min);
-			writeln(archivo_texto, cel_aux.stock_disp);
+				writeln(archivo_texto,' ',cel_aux.codigo, ' ',cel_aux.stock_min, ' ',cel_aux.desc);
+				writeln(archivo_texto, ' ',cel_aux.stock_disp, ' ',cel_aux.precio, ' ',cel_aux.nombre);
+			writeln(archivo_texto, ' ',cel_aux.marca);
 		end;
 		close(archivo_texto);
 		close(arch);
@@ -124,6 +124,51 @@ procedure agregarInformacion(var arch: archivocel);
 				write(arch, cel_aux);
 			end;
 		end;
+		close(arch);
+	end;
+	
+{Inciso F}
+procedure modificarStock(var arch: archivocel);
+	var
+		cel_aux: celular;
+		user_input: string[20];
+		user_input2: integer;
+	begin
+		reset(arch);
+		write('Ingrese el nombre del celular de cual busca modificar el stock: '); readln(user_input);
+		read(arch, cel_aux);
+		while (user_input <> cel_aux.nombre) do begin
+			read(arch, cel_aux);
+		end;
+		if (user_input <> cel_aux.nombre) then begin
+			writeln('No se pudo encontrar un celular bajo ese nombre');
+		end else begin
+			write('Ingrese el nuevo numero de stock: '); readln(user_input2);
+			cel_aux.stock_disp:= user_input2;
+			seek(arch, filepos(arch)-1);
+			write(arch, cel_aux); //Sobreescribir registro
+		end;
+		close(arch);
+	end;
+
+{Inciso G}
+procedure exportarSinStock(var arch: archivocel);
+	var
+		archivo_texto: text;
+		cel_aux: celular;
+	begin
+		Assign(archivo_texto, 'SinStock.txt');
+		reset(arch);
+		rewrite(archivo_texto);
+		while not eof(arch) do begin
+			read(arch, cel_aux);
+			if (cel_aux.stock_disp = 0) then begin
+				writeln(archivo_texto,' ',cel_aux.codigo, ' ',cel_aux.stock_min, ' ',cel_aux.desc);
+				writeln(archivo_texto, ' ',cel_aux.stock_disp, ' ',cel_aux.precio, ' ',cel_aux.nombre);
+				writeln(archivo_texto, ' ',cel_aux.marca);
+			end;
+		end;
+		close(archivo_texto);
 		close(arch);
 	end;
 
@@ -149,12 +194,14 @@ begin
 		writeln('>>3 - Listar celulares con tal descripción ');
 		writeln('>>4 - Exportar archivo (texto) ');
 		writeln('>>5 - Agregar informacion ');
+		writeln('>>6 - Modificar el stock de un celular dado ');
+		writeln('>>7 - Exportar archivo con aquellos celulares que no tengan stock (texto) ');
 		writeln;
 		write('Que opcion elige: '); readln(menu_input);
 		
 		// Comprobar el input
 		if (menu_input <> 0) then begin
-			if (menu_input < 6) and (menu_input > 0) then begin
+			if (menu_input < 8) and (menu_input > 0) then begin
 				writeln;
 				case menu_input of
 					1: crearArchivo(archivo);
@@ -162,6 +209,8 @@ begin
 					3: listarCelulares(archivo);
 					4: exportarArchivo(archivo);
 					5: agregarInformacion(archivo);
+					6: modificarStock(archivo);
+					7: exportarSinStock(archivo);
 				end;
 			end else begin
 				writeln('Lo sentimos, esa opcion no existe');
