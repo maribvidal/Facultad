fd1b1a1696600adfcc7d2ebbb797671c94641302
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-03-25 14:45:57 -0300

    Subido

---
+++ Tercer semestre/Algoritmos y Estructuras de Datos/Práctica 1/ej7a.java
@@ -0,0 +1,20 @@
+package ejercicio7;
+
+import java.util.ArrayList;
+
+public class TestArrayList {
+	public class LinkedList<Estudiante> generarLista(LinkedList<Estudiante> listaOriginal) {
+		LinkedList<Estudiante> nuevaLista;
+		return nuevaLista;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> listaNumeros = new ArrayList<Integer>();
+		//Agregar numeros del 1 a N
+		for (int i = 0; i < 10; i++) listaNumeros.add(i);
+		//Recorrer ArrayList con un F
+		for (int i: listaNumeros) {
+			System.out.println("Número: " + i);
+		}
+	}
+}
