3f18b0bb76b207b666efbf3b46c433475aaafda7
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-04-10 20:20:47 -0300

    Subido

---
+++ Tercer semestre/Seminario .NET/Práctica 5/Ejercicio 1/Program.cs
@@ -0,0 +1,10 @@
+using teoria5;
+
+Cuenta c1 = new Cuenta();
+c1.Depositar(100).Depositar(50).Extraer(120).Extraer(50);
+Cuenta c2 = new Cuenta();
+c2.Depositar(200).Depositar(800);
+new Cuenta().Depositar(20).Extraer(20);
+c2.Extraer(1000).Extraer(1);
+Console.WriteLine("\nDETALLE");
+Cuenta.ImprimirDetalle();
