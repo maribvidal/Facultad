b88cd4bbefd899f8a97fee2558d2884215084d72
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-04-12 17:16:47 -0300

    Actualizado

--- Tercer semestre/Seminario .NET/Práctica 5/Ejercicio 8/Program.cs
+++ Tercer semestre/Seminario .NET/Práctica 5/Ejercicio 8/Program.cs
@@ -1,15 +1,20 @@
 using teoria5;
 
-DateTime mismaFecha = new DateTime(2000, 6, 12);
 ListaDePersonas lista = new ListaDePersonas();
+
+DateTime unaFecha = new DateTime(2000, 6, 12);
+DateTime otraFecha = new DateTime(2004, 1, 2);
+DateTime terceraFecha = new DateTime(2002, 3, 4);
+
 Persona sujeto1 = new Persona();
 Persona sujeto2 = new Persona();
 Persona sujeto3 = new Persona();
 Persona sujeto4 = new Persona();
-sujeto1[0] = "aA"; sujeto1[1] = 'M'; sujeto1[2] = 123; sujeto1[3] = mismaFecha;
-sujeto2[0] = "aB"; sujeto2[1] = 'M'; sujeto2[2] = 234; sujeto2[3] = mismaFecha;
-sujeto3[0] = "aC"; sujeto3[1] = 'F'; sujeto3[2] = 345; sujeto3[3] = mismaFecha;
-sujeto4[0] = "D"; sujeto4[1] = 'F'; sujeto4[2] = 456; sujeto4[3] = mismaFecha;  
+
+sujeto1[0] = "Maria"; sujeto1[1] = 'F'; sujeto1[2] = 123; sujeto1[3] = unaFecha;
+sujeto2[0] = "Manuel"; sujeto2[1] = 'M'; sujeto2[2] = 234; sujeto2[3] = otraFecha;
+sujeto3[0] = "Carlos"; sujeto3[1] = 'M'; sujeto3[2] = 345; sujeto3[3] = terceraFecha;
+sujeto4[0] = "Clara"; sujeto4[1] = 'F'; sujeto4[2] = 456; 
 
 lista.Agregar(sujeto1);
 lista.Agregar(sujeto2);
@@ -17,10 +22,10 @@ lista.Agregar(sujeto3);
 lista.Agregar(sujeto4);
 
 //Probar código
-Persona unaPersona = lista[345];
-unaPersona.Imprimir();
-List<string> unaLista = lista['a'];
+Persona unaPersona = lista[456];
+unaPersona?.Imprimir();
+
+//Obtener lista de personas que llevan una 'M' en su nombre
+List<string> unaLista = lista['M'];
 foreach (string nom in unaLista)
-{
     Console.WriteLine(nom);    
-}
