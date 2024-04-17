965654a354e048bfeb4be991dd59a8aacef12283
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-04-16 21:08:29 -0300

    Subido

---
+++ Tercer semestre/Seminario .NET/Práctica 6/Ejercicio 8/Empleado.cs
@@ -0,0 +1,24 @@
+//No se podrán crear objetos de la clase padre Empleado
+abstract public class Empleado 
+{
+    public string Nombre {get;}
+    public int DNI {get;}
+    public DateTime FechaDeIngreso {get;}
+    public double SalarioBase {get;set;} //No puede ser solo de lectura
+    public abstract double Salario {get;} //Sobreescribirla desde clases derivadas
+
+    //Constructor
+    public Empleado(string nombre, int dni, DateTime fechaDeIngreso, double salarioBase)
+    {
+        Nombre = nombre;
+        DNI = dni;
+        FechaDeIngreso = fechaDeIngreso;
+        SalarioBase = salarioBase;
+    }
+
+    //Métodos
+    public abstract void AumentarSalario();
+    public override string ToString() {
+        return $"Nombre: {Nombre}, DNI: {DNI} Antigüedad: {2022 - FechaDeIngreso.Year}\nSalario base: {SalarioBase}, Salario: {Salario}\n-------------";
+    }
+}
