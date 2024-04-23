f598c07c51ec2e8f823785d54a0dc607fc394971
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-04-23 18:50:38 -0300

    Actualizado

--- Tercer semestre/Seminario .NET/Práctica 7/Ejercicio 1/Programa.cs
+++ Tercer semestre/Seminario .NET/Práctica 7/Ejercicio 1/Programa.cs
@@ -1,22 +1,28 @@
 using teoria7;
 
-//Crear objetos
-Auto auto = new Auto();
-Libro libro = new Libro();
-Persona persona = new Persona();
-Perro perro = new Perro();
-Pelicula pelicula = new Pelicula();
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Crear objetos
+        Auto auto = new Auto();
+        Libro libro = new Libro();
+        Persona persona = new Persona();
+        Perro perro = new Perro();
+        Pelicula pelicula = new Pelicula();
 
-//Operaciones
-Procesador.Alquilar(pelicula, persona);
-Procesador.Alquilar(libro, persona);
-Procesador.Atender(persona);
-Procesador.Atender(perro);
-Procesador.Devolver(pelicula, persona);
-Procesador.Devolver(libro, persona);
-Procesador.Lavar(auto);
-Procesador.Reciclar(libro);
-Procesador.Reciclar(auto);
-Procesador.Secar(auto);
-Procesador.Vender(auto, persona);
-Procesador.Vender(perro, persona);
+        //Operaciones
+        Procesador.Alquilar(pelicula, persona);
+        Procesador.Alquilar(libro, persona);
+        Procesador.Atender(persona);
+        Procesador.Atender(perro);
+        Procesador.Devolver(pelicula, persona);
+        Procesador.Devolver(libro, persona);
+        Procesador.Lavar(auto);
+        Procesador.Reciclar(libro);
+        Procesador.Reciclar(auto);
+        Procesador.Secar(auto);
+        Procesador.Vender(auto, persona);
+        Procesador.Vender(perro, persona);
+    }
+}
