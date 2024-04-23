75a1cff53fc83d2cfe72f6ff150241070578bdae
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-04-23 18:56:57 -0300

    Subido

---
+++ Tercer semestre/Seminario .NET/Práctica 7/Ejercicio 2/Programa.cs
@@ -0,0 +1,32 @@
+using teoria7;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Crear objetos
+        Auto auto = new Auto();
+        Libro libro = new Libro();
+        Persona persona = new Persona();
+        Perro perro = new Perro();
+        Pelicula pelicula = new Pelicula();
+
+        //Operaciones
+        Procesador.Alquilar(pelicula, persona);
+        Procesador.Alquilar(libro, persona);
+        Procesador.Atender(persona);
+        Procesador.Atender(perro);
+        Procesador.Devolver(pelicula, persona);
+        Procesador.Devolver(libro, persona);
+        Procesador.Lavar(auto);
+        Procesador.Reciclar(libro);
+        Procesador.Reciclar(auto);
+        Procesador.Secar(auto);
+        Procesador.Vender(auto, persona);
+        Procesador.Vender(perro, persona);
+        PeliculaClasica peliculaClasica = new PeliculaClasica();
+        Procesador.Alquilar(peliculaClasica, persona);
+        Procesador.Devolver(peliculaClasica, persona);
+        Procesador.Vender(peliculaClasica, persona);
+    }
+}
