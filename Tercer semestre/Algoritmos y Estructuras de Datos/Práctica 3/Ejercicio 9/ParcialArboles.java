101d9a6d29cfded50d446efc80cfb7a54018e6e7
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-04-30 22:01:16 -0300

    Actualizado

--- Tercer semestre/Algoritmos y Estructuras de Datos/Práctica 3/Ejercicio 9/ParcialArboles.java
+++ Tercer semestre/Algoritmos y Estructuras de Datos/Práctica 3/Ejercicio 9/ParcialArboles.java
@@ -2,6 +2,7 @@ package tp3;
 
 public class ParcialArboles {
 	//Ejercicio 9
+	//Recursiva
 	public static boolean esDeSeleccion(GeneralTree<Integer> arbol) {
 		return (!arbol.isEmpty() && comprobarSiEsDeSeleccion(arbol)) ? true : false;
 	}
@@ -17,5 +18,26 @@ public class ParcialArboles {
 				devolverValor = false;
 		}
 		return devolverValor;
+	}
+
+	//Forma iterativa (solución óptima)
+	public static boolean esDeSeleccion(GeneralTree<Integer> arbol) {
+		boolean esDeSeleccion = true;
+		if (!arbol.isEmpty()) {
+			Queue<GeneralTree<Integer>> cola = new Queue<GeneralTree<Integer>>();
+			GeneralTree<Integer> arbolAux = null;
+			
+			cola.enqueue(arbol);
+			while (!cola.isEmpty() && esDeSeleccion) {
+				int valorMin = Integer.MAX_VALUE;
+				arbolAux = cola.dequeue();
+				for (GeneralTree<Integer> hijo : arbolAux.getChildren()) {
+					cola.enqueue(hijo);
+					valorMin = Math.min(valorMin, hijo.getData());
+				}
+				if (!arbolAux.isLeaf() && arbolAux.getData() != valorMin) {esDeSeleccion = false;}
+			}
+		}
+		return esDeSeleccion;
 	}
 }
