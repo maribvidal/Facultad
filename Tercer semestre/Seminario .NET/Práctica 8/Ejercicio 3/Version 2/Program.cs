c6123dc81d2feba587a56cb9bdeeb339722cf4af
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-05-07 22:15:56 -0300

    Subido

--- Tercer semestre/Seminario .NET/Práctica 8/Ejercicio 3/Version 2/Program.cs
+++ Tercer semestre/Seminario .NET/Práctica 8/Ejercicio 3/Version 2/Program.cs
@@ -1,9 +1,9 @@
 using teoria7;
 using System.Collections;
 
-IEnumerable rango = new Rango(6, 30, 3);
-IEnumerable potencias = new Potencias(2, 10);
-IEnumerable divisibles = new DivisiblesPor(rango, 6);
+IEnumerable rango = Rango(6, 30, 3);
+IEnumerable potencias = Potencias(2, 10);
+IEnumerable divisibles = DivisiblesPor(rango, 6);
 
 foreach (int i in rango)
 {
@@ -22,3 +22,25 @@ foreach (int i in divisibles)
     Console.Write(i + " ");
 }
 Console.WriteLine();
+
+//Habemus enumeradores
+IEnumerable Rango(int i, int j, int p)
+{
+    for (int k = i; k <= j; k += p)
+        yield return k;
+}
+
+IEnumerable Potencias(int b, int k)
+{
+    for (int i = 0; i < k; i++)
+        yield return ((int) Math.Pow(2, i+1));
+}
+
+IEnumerable DivisiblesPor(IEnumerable e, int i)
+{
+    foreach (int num in e)
+    {
+        if (num % i == 0)
+            yield return num;
+    }
+}
