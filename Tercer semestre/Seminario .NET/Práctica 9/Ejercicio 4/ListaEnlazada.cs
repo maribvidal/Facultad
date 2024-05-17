76c3d687ec95379cd6a1afcc9b9043a7e368e31a
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-05-16 22:43:09 -0300

    Subido

--- Tercer semestre/Seminario .NET/Práctica 9/Ejercicio 4/ListaEnlazada.cs
+++ Tercer semestre/Seminario .NET/Práctica 9/Ejercicio 4/ListaEnlazada.cs
@@ -3,18 +3,38 @@ using System.Collections;
 
 class ListaEnlazada<T>
 {
-    List<T> _lista = new List<T>();
+    List<Nodo<T>> _lista = new List<Nodo<T>>();
 
-    public void AgregarAdelante(T miDato)
+    private Nodo<T> CrearNodo(T valor)
     {
-        _lista.Insert(0, miDato);
+        return new Nodo<T>(valor);
+    }
+
+    public void AgregarAdelante(T valor)
+    {
+        Nodo<T> miNodo = CrearNodo(valor);
+
+        //Vincular el nodo actual con el siguiente en la lista, si es que hay uno
+        if (_lista.Count == 0)
+            miNodo.Proximo = null;
+        else
+            miNodo.Proximo = _lista[0];
+
+        _lista.Insert(0, miNodo);
     } 
-    public void AgregarAtras(T miDato)
+    public void AgregarAtras(T valor)
     {
-        _lista.Add(miDato);
+        Nodo<T> miNodo = CrearNodo(valor);
+
+        //Vincular el ante-último nodo de la lista con el último, si es que existen nodos
+        if (_lista.Count == 0)
+            _lista[_lista.Count - 1].Proximo = miNodo;
+
+        _lista.Add(miNodo);
     }
     public IEnumerator<T> GetEnumerator()
     {
-        return _lista.GetEnumerator();
+        foreach (Nodo<T> unNodo in _lista)
+            yield return unNodo.Valor;
     }
 }
