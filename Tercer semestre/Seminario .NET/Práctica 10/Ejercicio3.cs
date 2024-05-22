9473f36a36a3c83c0e6aaada1c728b2535dc8d26
Mariano B. Vidal <118082350+MarianoBVidal@users.noreply.github.com>
2024-05-22 13:44:51 -0300

    Actualizado

--- Tercer semestre/Seminario .NET/Práctica 10/Ejercicio3.cs
+++ Tercer semestre/Seminario .NET/Práctica 10/Ejercicio3.cs
@@ -6,6 +6,9 @@ using Escuela;
 using var db = new EscuelaContext(); // Crear objeto de contexto
 
 // USING
+
+// INCISO A
+
 //Esta consulta obtiene el nombre de los alumnos que figuran en la tabla de examenes (se encuentran repetidos)
 var query = db.Alumnos.Join(db.Examenes,
                             a => a.AlumnoId,
@@ -22,4 +25,40 @@ query2.Where(a => a.Examenes.Count > 0)
     .OrderBy(nom => nom) //Ordenar por nombre
     .ToList() //Pasarlo a lista para así poder utilizar ForEach()
     .ForEach(alum => Console.WriteLine(alum.Nombre));
+
+//      INCISO B
+
+var query2 = db.Cursos.Where(c => c.Examenes.Count > 0)
+                      .Select(c => new
+                            {
+                                Título = c.Titulo,
+                                Cantidad = c.Examenes.Count
+                            })
+                      .OrderBy(obj => obj.Cantidad);
+query2.ToList().ForEach(obj => Console.WriteLine(obj));
+
+Console.WriteLine();
+
+//      INCISO C
+
+var query3 = db.Alumnos.Join(db.Examenes,
+                            a => a.AlumnoId,
+                            e => e.AlumnoId,
+                            (a, e) => new
+                            {
+                                Alumno = a.Nombre,
+                                Curso = e.CursoId,
+                                Nota = e.Nota
+                            });
+var query3dos = db.Cursos.Join(query3,
+                              c => c.CursoId,
+                              q => q.Curso,
+                              (c, q) => new
+                              {
+                                Alumno = q.Alumno,
+                                Curso = c.Titulo,
+                                Nota = q.Nota
+                              });
+query3dos.OrderBy(obj => obj.Alumno).ToList().ForEach(obj => Console.WriteLine(obj));
+
 // USING
