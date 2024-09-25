ea93a58ea717f80545b4bef8ef17a79ec19d6c19
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-09-24 21:30:30 -0300

    Actualizado

--- Cuarto Semestre/OO1/Cuadernillo/Ejercicio 9/CajaDeAhorro.java
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 9/CajaDeAhorro.java
@@ -1,16 +1,31 @@
-package ar.unlp.tendler.ejercicio9;
+package ar.edu.unlp.oo1.ejercicio9;
 
 public class CajaDeAhorro extends Cuenta {
-    double comision = 1.02;
+    double comision = 0.02;
+    
+    public CajaDeAhorro(double saldo) {
+    	super.depositar(saldo);
+    }
     
     @Override
     public boolean extraer(double monto) {
-        return super.extraer(monto * comision);
+        return super.extraer(monto + monto * comision);
     }
     
     @Override
     public boolean transferirACuenta(double monto, Cuenta cuentaDestino) {
-        return super.transferirACuenta(monto * comision, cuentaDestino);
+    	if (puedeExtraer(monto * comision)) {
+    		super.transferirACuenta(monto, cuentaDestino);
+    		this.saldo -= monto * comision;
+    		return true;
+    	}
+    	return false;
+    }
+    
+    @Override
+    public void depositar(double monto) {
+    	// Se cobra la comisión al depositar
+    	super.depositar(monto - (monto * comision));
     }
     
     public boolean puedeExtraer(double monto) {
