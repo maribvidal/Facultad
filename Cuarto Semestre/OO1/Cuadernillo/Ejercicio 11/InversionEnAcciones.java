83731d242ce4a4963704418bbb621b44fc2dc041
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-09-27 09:28:46 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 11/InversionEnAcciones.java
@@ -0,0 +1,33 @@
+package ar.edu.unlp.oo1.ejercicio11;
+
+public class InversionEnAcciones implements IInversion {
+	String nombre;
+	int cantidad;
+	double valorUnitario;
+	
+	public InversionEnAcciones(String nombre) {
+		this.nombre = nombre;
+		this.setCantidad(0);
+		this.setValorUnitario(0);
+	}
+	
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public double valorActual() {
+		return cantidad * valorUnitario;
+	}
+}
