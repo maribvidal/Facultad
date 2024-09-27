d03a8d30ef19b05b9805c58e5befae57e1f28f77
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-09-27 09:28:04 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 11/PlazoFijo.java
@@ -0,0 +1,38 @@
+package ar.edu.unlp.oo1.ejercicio11;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class PlazoFijo implements IInversion {
+	LocalDate fechaDeConstitucion;
+	double montoDepositado;
+	double porcentajeDeInteresDiario;
+	
+	public PlazoFijo(LocalDate fecha) {
+		this.fechaDeConstitucion = fecha;
+		this.setMontoDepositado(0);
+		this.setPorcentajeDeInteresDiario(1);
+	}
+	
+	public double getMontoDepositado() {
+		return montoDepositado;
+	}
+
+	public void setMontoDepositado(double montoDepositado) {
+		this.montoDepositado = montoDepositado;
+	}
+
+	public double getPorcentajeDeInteresDiario() {
+		return porcentajeDeInteresDiario;
+	}
+
+	public void setPorcentajeDeInteresDiario(double porcentajeDeInteresDiario) {
+		this.porcentajeDeInteresDiario = porcentajeDeInteresDiario;
+	}
+
+	public double valorActual() {
+		long cantidadDias = ChronoUnit.DAYS.between(fechaDeConstitucion, LocalDate.now());
+		double interes = cantidadDias * (porcentajeDeInteresDiario * montoDepositado);
+		return montoDepositado + interes;
+	}
+}
