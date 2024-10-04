993fd6d073b8517c3222919dc6544f6af9c1c9a0
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-10-03 22:06:32 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 12/ReporteDeConstruccion.java
@@ -0,0 +1,20 @@
+package ar.edu.unlp.oo1.ejercicio12;
+
+import java.util.List;
+
+public class ReporteDeConstruccion {
+	List<Pieza> piezas;
+	
+	// Métodos
+	public void agregarPieza(Pieza pieza) { this.piezas.add(pieza); }
+	
+	public double volumenDeMaterial(String material) {
+		double total = this.piezas.stream().filter(p -> p.material == material).mapToDouble(p -> p.getVolumen()).sum();
+		return total;
+	}
+	
+	public double superficieDeColor(String color) {
+		double total = this.piezas.stream().filter(p -> p.color == color).mapToDouble(p -> p.getSuperficie()).sum();
+		return total;
+	}
+}
