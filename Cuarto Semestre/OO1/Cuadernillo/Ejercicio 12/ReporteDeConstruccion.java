a274815ab2700547d536b9af360a732aea36521a
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-10-03 22:11:58 -0300

    Actualizado

--- Cuarto Semestre/OO1/Cuadernillo/Ejercicio 12/ReporteDeConstruccion.java
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 12/ReporteDeConstruccion.java
@@ -1,9 +1,12 @@
 package ar.edu.unlp.oo1.ejercicio12;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ReporteDeConstruccion {
 	List<Pieza> piezas;
+	
+	public ReporteDeConstruccion() { this.piezas = new LinkedList<Pieza>(); }
 	
 	// Métodos
 	public void agregarPieza(Pieza pieza) { this.piezas.add(pieza); }
