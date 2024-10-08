8a6b16e91274098af25a60a7369b57b7839ccd39
Mariano B. Vidal <marianobazalarvidal.229958@alumnos.info.unlp.edu.ar>
2024-10-08 18:36:19 -0300

    Actualizado: lo hice bien esta vez

--- Cuarto Semestre/ISO/Práctica 3/Script14.sh
+++ Cuarto Semestre/ISO/Práctica 3/Script14.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
-numArgs=$#
-
-# Se deberán recibir 3 argumentos:
-# - El primero, una opción -[ab].
-# - El segundo, la ruta del fichero.
-# - El tercero, la cadena a concatenar.
-# Se debe comprobar si la ruta 
-# pasada corresponde a un fichero
-
-if [[ $numArgs -eq 3 && -f "$2" ]]; then
-        # Obtener la ruta sin nombre de fichero
-        # y el nombre de fichero sin ruta
-        #
-        rutaDir=$(dirname "$2")
-        nomFichero=$(basename "$2")
-        # Leer las opciones
-        while getopts ":ab" opcion; do
-                case "$opcion" in
-                        a) nuevaRuta="${rutaDir}/${nomFichero}$3"
-                           mv $2 $nuevaRuta
-                           ;;
-                        b) nuevaRuta="${rutaDir}/$3${nomFichero}"
-                           mv $2 $nuevaRuta
-                           ;;
-                        \?) echo "-$DPTARG: OPCIÓN INVÁLIDA - "
-                           exit 1;;
-                esac
-        done
-else
-        echo " - ERROR: Se esperaban 3 argumentos / una ruta de fichero válida"
+# Verificar que se ha pasado un argumento
+if [ $# -ne 3 ]; then
+        echo "ERROR: Faltan pasar los argumentos"
         exit 1
 fi
-echo "- ACCIÓN REALIZADA CON ÉXITO"
-echo "- Nueva ruta del archivo: $nuevaRuta"
-exit 0
-                                                                                                 24,42-63     T
+
+# Verificar que el primer argumento pasado sea un directorio
+if ! [ -d $1 ]; then
+        echo "ERROR: Directorio no válido"
+        exit 1
+fi
+
+# Renombrar solo los archivos del directorio pasado
+archivos_dir=$(ls -h $1)
+
+case $2 in
+        "-a")
+                for arch in $archivos_dir; do
+                        mv "$1/$arch" "$1/$arch$3"
+                done
+                ;;
+        "-b")
+                for arch in $archivos_dir; do
+                        mv "$1/$arch" "$1/$3$arch"
+                done
+                ;;
+        *)
+                echo "ERROR: Opción inválida"
+                exit 1
+                ;;
+esac
