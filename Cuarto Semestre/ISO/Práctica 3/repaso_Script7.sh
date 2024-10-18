b55d4bee1f2e6688f504bf75bea26f13a846b461
Mariano B. Vidal <marianobazalarvidal.229958@alumnos.info.unlp.edu.ar>
2024-10-17 22:00:03 -0300

    Subido

---
+++ Cuarto Semestre/ISO/Práctica 3/repaso_Script7.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ $# -lt 3 ]; then
+        exit 1
+fi
+
+if ! [ -d $1 ]; then
+        exit 2
+fi
+
+function agregarArchivosDirectorio {
+        for arch in $(ls -h $1); do
+                test -f $1/$arch && arr+=($arch)
+        done
+}
+
+function eliminarArchivosPatron {
+        count=0
+        for elem in ${arr[@]}; do
+                if [ $(echo $elem | grep -c $1 ) -eq 1 ]; then
+                        unset arr[$count]
+                        if [[ $2 = "-f" ]]; then
+                                touch -f $dirPrimera/$elem && rm $dirPrimera/$elem
+                        fi
+                fi
+                count=$(expr $count + 1)
+        done
+}
+
+echo $*
+
+if [[ $2 = "-a" || $2 = "-d" ]]; then
+        arr=()
+        agregarArchivosDirectorio $1
+        dirPrimera=$1
+        if [ $2 = "-a" ]; then
+                dirSegunda=$3
+                agregarArchivosDirectorio $3
+        elif [ $2 = "-d" ]; then
+                eliminarArchivosPatron $3 $4
+        else
+                exit 1
+        fi
+        echo ${arr[@]}
+        exit 0
+fi
