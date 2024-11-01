e19d6a03c85a721d892e18209ffa5519ebf80027
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-10-31 21:16:30 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 18/PoliticaCancelacion.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.oo1.ejercicio17;
+
+import java.time.LocalDate;
+
+public abstract class PoliticaCancelacion {
+	public abstract double reembolsar(double montoOriginal, LocalDate fechaInicioReserva);
+}
