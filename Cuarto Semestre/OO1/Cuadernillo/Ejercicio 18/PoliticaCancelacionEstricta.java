b1c6e6ee5e600db6a0a685b4ec1553fcc395b9d1
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-10-31 21:17:50 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 18/PoliticaCancelacionEstricta.java
@@ -0,0 +1,9 @@
+package ar.edu.unlp.oo1.ejercicio17;
+
+import java.time.LocalDate;
+
+// quizás es medio inutil crear esta clase
+
+public class PoliticaCancelacionEstricta extends PoliticaCancelacion {
+	public double reembolsar(double montoOriginal, LocalDate fechaCancelacion) { return 0; }
+}
