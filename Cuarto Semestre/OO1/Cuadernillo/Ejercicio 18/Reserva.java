522dc6adcc2fbaa823db63eb440bdf23ba21f2f0
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-10-31 21:15:45 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 18/Reserva.java
@@ -0,0 +1,23 @@
+package ar.edu.unlp.oo1.ejercicio17;
+
+// Esta clase se controla desde Usuario
+
+public class Reserva {
+	public Propiedad propiedad;
+	public DateLapse periodo;
+	
+	public Reserva(Propiedad propiedad, DateLapse periodo) {
+		this.propiedad = propiedad;
+		this.periodo = periodo;
+	}
+	
+	// El usuario debe poder calcular el precio de la reserva
+	public double calcularPrecio()
+	{
+		return this.propiedad.consultarPrecioPeriodo(periodo);
+	}
+	
+	public double obtenerReembolso() {
+		return this.propiedad.obtenerReembolso(periodo);
+	}
+}
