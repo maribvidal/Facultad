024c95ef6cf224825df8e99567d6661c7b68fb67
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-11-08 10:22:40 -0300

    Actualizado

--- Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/Producto.java
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/Producto.java
@@ -1,8 +1,21 @@
-package ar.edu.unlp.info.oo1.ejercicio23;
+package ar.edu.unlp.info.oo1.Ejercicio23;
 
 public class Producto {
 	private String nombre;
 	private String categoria;
 	private double precio;
 	private int unidades;
+	
+	public int getUnidades() {
+		return this.unidades;
+	}
+	public void setUnidades(int num) {
+		this.unidades = num;
+	}
+	public String getCategoria() {
+		return this.categoria;
+	}
+	public double getPrecio() {
+		return this.precio;
+	}
 }
