1a1e219bcac2525f9905dfea02caabd9ff5f2e62
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-11-08 10:25:18 -0300

    Actualizado

--- Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/IFormaDePago.java
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/IFormaDePago.java
@@ -1,5 +1,5 @@
-package ar.edu.unlp.info.oo1.ejercicio23;
+package ar.edu.unlp.info.oo1.Ejercicio23;
 
 public interface IFormaDePago {
-	double calcularPrecioFinal();
+	double calcularPrecioFinal(double precio);
 }
