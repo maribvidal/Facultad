d3335bcc71571c9fdfe39b5d8ae7fefbc4a92251
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-11-08 10:22:25 -0300

    Actualizado

--- Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/Pedido.java
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/Pedido.java
@@ -1,12 +1,27 @@
-package ar.edu.unlp.info.oo1.ejercicio23;
+package ar.edu.unlp.info.oo1.Ejercicio23;
 
 public class Pedido {
-	private int cantidadSolicitada;
-	private Producto producto;
-	private IFormaDeEnvio formaDeEnvio;
-	private IFormaDePago formaDePago;
+	Producto producto;
+	IFormaDePago formaDePago;
+	IFormaDeEnvio formaDeEnvio;
+	int cantidadSolicitada;
 	
-	public double calcularCostoTotal() {
-		return 0;
+	public Pedido(Producto producto, IFormaDeEnvio formadeenvio, IFormaDePago formadepago, int cant) {
+		this.producto = producto;
+		this.formaDeEnvio = formadeenvio;
+		this.formaDePago = formadepago;
+		this.cantidadSolicitada = cant;
+	}
+	
+	public double calcularCostoTotal () {
+		double costoTotal = 0;
+		//(precio final en base a la forma de pago seleccionada) + (costo de envío en base a la forma de envío seleccionada).
+		double precioProd = this.cantidadSolicitada * producto.getPrecio();
+		costoTotal = this.formaDePago.calcularPrecioFinal(precioProd) + this.formaDeEnvio.calcularCostoEnvio();
+		return costoTotal;
+	}
+	
+	public String getCategoria() {
+		return this.producto.getCategoria();
 	}
 }
