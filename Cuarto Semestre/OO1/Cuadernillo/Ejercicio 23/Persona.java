bb74939baa9d76c254df78d2f63ba0b7889bf541
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-11-08 10:22:04 -0300

    Actualizado

--- Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/Persona.java
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/Persona.java
@@ -1,6 +1,7 @@
-package ar.edu.unlp.info.oo1.ejercicio23;
+package ar.edu.unlp.info.oo1.Ejercicio23;
 
 public abstract class Persona {
-	private String nombre;
-	private String apellido;
+	protected String nombre;
+	protected String apellido;
+	protected String direccion;
 }
