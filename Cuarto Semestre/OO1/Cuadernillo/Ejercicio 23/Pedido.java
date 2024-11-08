9c0027ccab335eda24740a430a066b2134722ede
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-11-08 09:30:03 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Cuadernillo/Ejercicio 23/Pedido.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.info.oo1.ejercicio23;
+
+public class Pedido {
+	private int cantidadSolicitada;
+	private Producto producto;
+	private IFormaDeEnvio formaDeEnvio;
+	private IFormaDePago formaDePago;
+	
+	public double calcularCostoTotal() {
+		return 0;
+	}
+}
