1b24b9713cc610d5d29eb36f43efc59b233f265f
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-11-17 20:21:41 -0300

    Subido

---
+++ Cuarto Semestre/OO1/Parcial 16-11-2024/Usuario.java
@@ -0,0 +1,61 @@
+package ar.edu.unlp.oo1.parcial;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.Period;
+
+// De cada usuario se conoce su nombre y las entradas que compró.
+
+public class Usuario {
+	private String nombre;
+	private List<Entrada> entradasCompradas;
+	
+	public Usuario(String nom) {
+		this.nombre = nom;
+		this.entradasCompradas = new ArrayList<Entrada>();
+	}
+	
+	// CONSULTAR EL PRECIO DE ASISTENCIA A UN EVENTO
+	public double consultarPrecioAsistenciaEvento(Evento evento, LocalDate fechaConsulta) {
+		return (evento.obtenerPrecioAsistencia(fechaConsulta));
+	}
+	
+	// COMPRAR UNA ENTRADA PARA UN EVENTO
+	// Al momento de comprar una entrada, se puede decir si
+	// pagar o no un seguro de reembolso que permite recuperar
+	// un mayor monto en caso de devolución de la entrada.
+	public Entrada comprarEntradaEvento(Evento evento, boolean quiereSeguro) {
+		Entrada nuevaEntrada = null;
+		
+		if (evento.getFecha().isAfter(LocalDate.now())) {
+			nuevaEntrada = new Entrada(evento, quiereSeguro);
+			this.entradasCompradas.add(nuevaEntrada);
+		}
+		
+		return nuevaEntrada;
+	}
+	
+	// CALCULAR EL MONTO A RECUPERAR POR UNA ENTRADA
+	public double calcularMontoRecuperableEntrada(Entrada entrada) {
+		return (entrada.obtenerMontoRecuperable());
+	}
+	
+	// CALCULAR EL MONTO TOTAL POR LAS ENTRADAS COMPRADAS EN UN PERIODO
+	public double calcularMontoTotalEntradasPeriodo(LocalDate fechaInicial, LocalDate fechaFinal) {
+		DateLapse periodo = new DateLapse(fechaInicial, fechaFinal);
+		double total = this.obtenerEntradasCompradasPeriodo(periodo).stream().mapToDouble(e -> e.obtenerMontoGastado()).sum();
+		return total;
+	}
+	
+	private List<Entrada> obtenerEntradasCompradasPeriodo(DateLapse periodo) {
+		return (this.entradasCompradas.stream().filter(e -> periodo.includesDate(e.getFechaCompra())).toList());
+	}
+	
+	// Retornar la entrada al siguiente evento
+	public Entrada obtenerEntradaSiguienteEvento() {
+		List<Entrada> entradasVigentes = this.entradasCompradas.stream().filter(e -> LocalDate.now().isBefore(e.getFechaEvento())).toList();
+		Entrada proximaEntrada = entradasVigentes.stream().min((e1, e2) -> Integer.compare(Period.between(LocalDate.now(), e1.getFechaEvento()).getDays(), Period.between(LocalDate.now(), e2.getFechaEvento()).getDays())).orElse(null);
+		return proximaEntrada;
+	}
+}
