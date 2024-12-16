bb3dae9d46c281c9aad2e9d819b646f858988f5f
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-12-16 20:32:19 -0300

    Correcciones aplicadas

--- Cuarto Semestre/OO1/Parcial 07-12-2024/Orco.java
+++ Cuarto Semestre/OO1/Parcial 07-12-2024/Orco.java
@@ -4,10 +4,9 @@ public class Orco extends Personaje {
 	// Al crear un orco, los valores iniciales de nivel, inteligencia
 	// y fuerza son los siguientes: 1, 1, 2
 	public Orco(String nom, IRol rol) {
-		this.nombre = nom;
-		this.nivel = 1;
-		this.habilidad = new Habilidad(1, 2);
-		this.rol = rol;
+		super(nom, rol);
+		this.inteligencia = 1;
+		this.fuerza = 2;
 	}
 	
 	// En cambio, para los orcos, se incrementa un 60% solamente sí es de noche
@@ -21,7 +20,7 @@ public class Orco extends Personaje {
 	public void subirNivel() {
 		this.nivel++;
 		if (this.nivelMultiploTres()) {
-			this.rol.incrementarHabilidad(this.nivel, this.habilidad);
+			this.rol.incrementarHabilidad(this);
 		}
 	}
 	
