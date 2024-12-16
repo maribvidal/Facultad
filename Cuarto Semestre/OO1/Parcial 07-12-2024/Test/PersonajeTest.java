0dd6b711d4dc7829452235e66f3e037d01cdddc6
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-12-16 20:33:56 -0300

    Actualizado

--- Cuarto Semestre/OO1/Parcial 07-12-2024/Test/PersonajeTest.java
+++ Cuarto Semestre/OO1/Parcial 07-12-2024/Test/PersonajeTest.java
@@ -1,7 +1,5 @@
 package parcial.segundaFecha;
 
-// Incompleto
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,10 +28,10 @@ public class PersonajeTest {
 	void test_inicializacion() {
 		// Comprobar la construcción de los humanos y orcos
 		Assertions.assertEquals(1, circe.getNivel());
-		Assertions.assertEquals(2, circe.getHabilidad().getInteligencia());
+		Assertions.assertEquals(2, circe.getInteligencia());
 		
 		Assertions.assertEquals(1, nadie.getNivel());
-		Assertions.assertEquals(2, nadie.getHabilidad().getFuerza());
+		Assertions.assertEquals(2, nadie.getFuerza());
 	}
 	
 	@Test
@@ -66,29 +64,29 @@ public class PersonajeTest {
 	void test_subirNivel_humano() {
 		// PARTICION NO.1: Nivel menor a 7 (no se modifican las habilidades) (rol mago)
 		
-		double valorViejo = circe.getHabilidad().getInteligencia();
+		double valorViejo = circe.getInteligencia();
 		// Subirle el nivel 5 veces para que sea 6 (valor de borde)
 		for (int i = 0; i < 5; i++) { circe.subirNivel(); }
-		Assertions.assertEquals(valorViejo, circe.getHabilidad().getInteligencia());
+		Assertions.assertEquals(valorViejo, circe.getInteligencia());
 		
 		// PARTICION NO.2: Nivel mayor o igual a 7 (se modifican las habilidades) (rol guerrero)
 		
 		for (int i = 0; i < 6; i++) { odiseo.subirNivel(); } // Nivel = 7
-		Assertions.assertEquals(1 + (7 / 6), odiseo.getHabilidad().getFuerza());
+		Assertions.assertEquals(1 + (7 / 6), odiseo.getFuerza());
 	}
 	
 	@Test
 	void test_subirNivel_orco() {
 		// PARTICION NO.1: Nivel no múltiplo de 3 (no se modifican las habilidades) (rol guerrero)
 		
-		double valorViejo = nadie.getHabilidad().getFuerza();
+		double valorViejo = nadie.getFuerza();
 		nadie.subirNivel(); // Nivel = 2
-		Assertions.assertEquals(valorViejo, nadie.getHabilidad().getFuerza());
+		Assertions.assertEquals(valorViejo, nadie.getFuerza());
 		
 		// PARTICION NO.2: Nivel múltiplo de 3 (se modifican las habilidades) (rol mago)
 		
 		polifemo.subirNivel();
 		polifemo.subirNivel(); // Nivel = 3
-		Assertions.assertEquals(1 + (3/2 * 3), polifemo.getHabilidad().getInteligencia());
+		Assertions.assertEquals(1 + (3/2 * 3), polifemo.getInteligencia());
 	}
 }
