1446e8ec2362ccb552bf4113ae4c220ffdf5be5d
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2024-12-16 20:32:35 -0300

    Correcciones aplicadas

--- Cuarto Semestre/OO1/Parcial 07-12-2024/Personaje.java
+++ Cuarto Semestre/OO1/Parcial 07-12-2024/Personaje.java
@@ -8,8 +8,15 @@ import java.util.List;
 public abstract class Personaje {
 	protected String nombre;
 	protected int nivel;
-	protected Habilidad habilidad;
+	protected double fuerza;
+	protected double inteligencia;
 	protected IRol rol;
+	
+	public Personaje(String nom, IRol rol) {
+		this.nombre = nom;
+		this.nivel = 1;
+		this.rol = rol;
+	}
 	
 	// MÉTODOS QUE VAN A TENER QUE IMPLEMENTAR LAS SUBCLASES
 	
@@ -46,6 +53,13 @@ public abstract class Personaje {
 	protected boolean poderAceptable() { return (this.determinarPoderAtaque(12) < 5); }
 	protected boolean diferenciaNivelAceptable(Personaje otro) { return (Math.abs(this.nivel - otro.nivel) <= 2); }
 	
+	public void aumentarInteligencia(double cant) { this.inteligencia += cant; }
+	public void aumentarFuerza(double cant) { this.fuerza += cant; }
+	
+	public double getInteligencia() { return this.inteligencia; }
+	public double getFuerza() { return this.fuerza; }
+	public int getNivel() { return this.nivel; }
+	
 	// Si el nivel del oponente es igual al del personaje dado, su poder de ataque
 	// a las 12 horas debe ser menor a 5. En otros casos, un oponente se considera
 	// de nivel similar si la diferencia entre su nivel y el del personaje dado es
@@ -57,9 +71,5 @@ public abstract class Personaje {
 	}
 	
 	protected boolean esDeDia(int hora) { return (hora >= 8 && hora < 20); }
-	protected double calcularValorBase() { return (this.rol.determinarValorBase(this.nivel, this.habilidad)); }
-	
-	// MÉTODOS DEBUG
-	public int getNivel() { return this.nivel; }
-	public Habilidad getHabilidad() { return this.habilidad; }
+	protected double calcularValorBase() { return (this.rol.determinarValorBase(this)); }
 }
