3d10a5e8c84865407e58798a9d814867d946cbf1
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2025-04-26 21:41:15 -0300

    Actualizado

--- 5to Semestre/OO2/Patrones/Ejercicio 13 - SubteWay/Test/SandwichTest.java
+++ 5to Semestre/OO2/Patrones/Ejercicio 13 - SubteWay/Test/SandwichTest.java
@@ -11,35 +11,45 @@ public class SandwichTest {
 	final int COSTO_ESPERADO_SANDWICH_SIN_TACC = 618;
 	
 	Sandwich sandwich;
+
+	SandwichClasicoBuilder constructorClasico;
+	SandwichVeganoBuilder constructorVegano;
+	SandwichVegetarianoBuilder constructorVegetariano;
+	SandwichSinTACCBuilder constructorSinTACC;
 	
 	Empleado empleadoSubteway;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		empleadoSubteway = new Empleado();
+
+		constructorClasico = new SandwichClasicoBuilder();
+		constructorVegano = new SandwichVeganoBuilder();
+		constructorVegetariano = new SandwichVegetarianoBuilder();
+		constructorSinTACC = new SandwichSinTACCBuilder();
 	}
 	
 	@Test
 	public void test_sandwich_clasico() {
-		sandwich = empleadoSubteway.pedirSandwichClasico();
+		sandwich = empleadoSubteway.pedirSandwich(constructorClasico);
 		Assertions.assertEquals(COSTO_ESPERADO_SANDWICH_CLASICO, sandwich.obtenerPrecio());
 	}
 	
 	@Test
 	public void test_sandwich_vegetariano() {
-		sandwich = empleadoSubteway.pedirSandwichVegetariano();
+		sandwich = empleadoSubteway.pedirSandwich(constructorVegetariano;
 		Assertions.assertEquals(COSTO_ESPERADO_SANDWICH_VEGETARIANO, sandwich.obtenerPrecio());
 	}
 	
 	@Test
 	public void test_sandwich_vegano() {
-		sandwich = empleadoSubteway.pedirSandwichVegano();
+		sandwich = empleadoSubteway.pedirSandwich(constructorVegano);
 		Assertions.assertEquals(COSTO_ESPERADO_SANDWICH_VEGANO, sandwich.obtenerPrecio());
 	}
 	
 	@Test
 	public void test_sandwich_sin_tacc() {
-		sandwich = empleadoSubteway.pedirSandwichSinTACC();
+		sandwich = empleadoSubteway.pedirSandwich(constructorSinTACC);
 		Assertions.assertEquals(COSTO_ESPERADO_SANDWICH_SIN_TACC, sandwich.obtenerPrecio());
 	}
 }
