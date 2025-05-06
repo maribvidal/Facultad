c17f6314f3edefb45a75a792e05e987e69cb07b7
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2025-05-06 12:08:58 -0300

    Subido

---
+++ 5to Semestre/OO2/Patrones/Ejercicio 16 - Filtros de Imágenes/imageFilters/Dull.java
@@ -0,0 +1,13 @@
+package unlp.oo2.patrones.ej16.imageFilters;
+
+import java.awt.image.BufferedImage;
+
+public class Dull extends Filter {
+  
+  
+    
+  public BufferedImage filter(BufferedImage image) {
+    
+    return image;
+  }
+}
