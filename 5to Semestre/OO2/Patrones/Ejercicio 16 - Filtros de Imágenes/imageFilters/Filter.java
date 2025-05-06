3a048d8f19ac5ca1aeb81970d1d34f1823b56c12
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2025-05-06 12:09:09 -0300

    Subido

---
+++ 5to Semestre/OO2/Patrones/Ejercicio 16 - Filtros de Imágenes/imageFilters/Filter.java
@@ -0,0 +1,7 @@
+package unlp.oo2.patrones.ej16.imageFilters;
+
+import java.awt.image.BufferedImage;
+
+public abstract class Filter {
+  abstract public BufferedImage filter(BufferedImage image);
+}
