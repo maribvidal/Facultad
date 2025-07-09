ee8dd8985eac419280854835e58f07a5d58175d8
mariano vidal <118082350+maribvidal@users.noreply.github.com>
2025-07-09 20:01:44 -0300

    Subido

--- 5to Semestre/OO2/Parcial 06-07-2025/ReportGeneratorTest.java
+++ 5to Semestre/OO2/Parcial 06-07-2025/ReportGeneratorTest.java
@@ -1,15 +1,30 @@
-class ReportGeneratorTest {
-  ReportGeneratorPDF generatorPDF;
-  ReportGeneratorXLS generatorXLS;
-  Document document;
+package refactoring070625;
 
-  @BeforeEach
-  void setUp() {
-    document = new Document("informe");
-    document.addAuthor("carlos");
-    document.addAuthor("ana");
-    generatorPDF = new ReportGeneratorPDF();
-    generatorXLS = new ReportGeneratorXLS();
-  }
-  ... // Lo demás igual
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ReportGeneratorTest {
+	ReportGenerator generatorPDF; 
+	ReportGenerator generatorXLS; 
+	Document document;
+	
+	@BeforeEach 
+	void setUp() { 
+		document = new Document("Informe"); 
+		document.addAuthor("Carlos");
+		document.addAuthor("Ana");
+		generatorPDF = new ReportGenerator("PDF"); 
+		generatorXLS = new ReportGenerator("XLS"); 
+	}
+
+	@Test 
+	void testPDF() { 
+		Assertions.assertEquals("salió bien", generatorPDF.generateReport(document));
+	}
+	
+	@Test 
+	void testXLS() { 
+		Assertions.assertEquals("salió bien", generatorXLS.generateReport(document));
+	}
 }
